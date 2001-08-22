1fdfa1c0a99e8f7bc990530f96e21ef3b83abb97
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2001-08-22 05:41:54 +0000

    First Import of ParserException

---
+++ trunk/OntoRama/src/ontorama/webkbtools/util/ParserException.java
@@ -0,0 +1,25 @@
+package ontorama.webkbtools.util;
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+import java.lang.Exception;
+
+public class ParserException extends Exception{
+  private String errorMsg = null;
+
+  public ParserException(String message) {
+    errorMsg = "\nParser failed:\n";
+    errorMsg = errorMsg + message;
+  }
+
+  public String getMessage() {
+    return errorMsg;
+  }
+
+}

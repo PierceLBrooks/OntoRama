4b2f9ee13a9a4b7bb0d6bb9efecdbe78c1831782
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2001-11-28 04:38:04 +0000

    First Import

---
+++ trunk/OntoRama/src/ontorama/tree/util/Debug.java
@@ -0,0 +1,26 @@
+
+
+package ontorama.tree.util;
+
+public class Debug {
+    /**
+     *
+     */
+     private boolean debug = false;
+
+    /**
+     *
+     */
+     public Debug (boolean debug) {
+        this.debug = debug;
+     }
+
+     /**
+      *
+      */
+      public void message (String className, String methodName, String message) {
+        if (debug) {
+            System.out.println(className + ", method " + methodName + ": " + message);
+        }
+      }
+}

bdfa1b98b46802a7d2a2dc1afa9995227e7d9b0f
sgreen <sgreen@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2001-12-12 01:41:41 +0000

    Label View no longer calculates font sizes on each redraw.
    Calculating font sizes on each redraw is quite expensive in
    Java. LabelView now has fixed font sizes to display label
    names.  This have increased the redraw performance
    greatly.

--- OntoRama/src/ontorama/hyper/view/simple/LabelView.java
+++ OntoRama/src/ontorama/hyper/view/simple/LabelView.java
@@ -11,6 +11,7 @@ import ontorama.hyper.view.simple.HyperNodeView;
 
 import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Paint;
@@ -27,15 +28,51 @@ public class LabelView extends CanvasItem{
     public LabelView( HyperNodeView hyperNodeView ) {
         this.hyperNodeView = hyperNodeView;
     }
+    /**
+     * The maximum number of fonts.
+     */
+    private static final int MAXFONTS = 10;
+
+    /**
+     * Stores the different size fonts to enable the labels
+     * to get smaller as the node move away from the center.
+     *
+     * Fonts sizes are stored to improve performance.
+     * Calculating the font sizes by using a scale factor
+     * seems to be very expencive in Java.
+     */
+    private static Font[] fonts = new Font[ MAXFONTS ];
+
+    /**
+     * Load new fonts into array.
+     */
+    static {
+        for( int i = 0; i < MAXFONTS; i++) {
+            fonts[i] = new Font("Arial", Font.PLAIN, i + 3);
+        }
+    }
+
+    /**
+     * Get the font size to display.
+     */
+    private Font getFontToDisplay( double scale ) {
+        if(scale == 1) {
+            return fonts[MAXFONTS-1];
+        }
+        int size = (int)((scale * MAXFONTS ) % MAXFONTS);
+        return fonts[size];
+    }
 
     public void draw( Graphics2D g2d ) {
         double scale = hyperNodeView.getScale();
+//        System.out.println( "scale: " + scale );
         if( scale < .1 ) {
             return;
         }
-        float fontSize = (float)(14 * scale);
-        g2d.setFont(g2d.getFont().deriveFont(fontSize));
-
+//      removed for now to improve performance
+//      float fontSize = (float)(14 * scale);
+//      g2d.setFont(g2d.getFont().deriveFont(fontSize));
+        g2d.setFont( getFontToDisplay( scale ) );
         FontMetrics fm = g2d.getFontMetrics();
         String label = this.hyperNodeView.getName();
         double x = hyperNodeView.getProjectedX();

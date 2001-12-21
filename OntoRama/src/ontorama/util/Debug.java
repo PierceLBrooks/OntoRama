8a9672000fef8171efb7ae43d357f16e6a63ae1e
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2001-12-21 01:16:54 +0000

    Debug class

---
+++ OntoRama/src/ontorama/util/Debug.java
@@ -0,0 +1,35 @@
+
+
+package ontorama.util;
+
+public class Debug {
+    /**
+     *
+     */
+     private boolean debug = false;
+
+    /**
+     *
+     */
+     public Debug (boolean debug) {
+        this.debug = debug;
+     }
+
+     /**
+      *
+      */
+      public void message (String className, String methodName, String message) {
+        if (debug) {
+            System.out.println(className + ", method " + methodName + ": " + message);
+        }
+      }
+
+      /**
+       *
+       */
+      public void message (String message) {
+        if (debug) {
+            System.out.println(message);
+        }
+      }
+}

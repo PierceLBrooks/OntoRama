1f006e081c407703c0fd65798c8e07d6b024dbde
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-01-03 01:19:54 +0000

    new method to return displayIcon

--- OntoRama/src/ontorama/ontologyConfig/RelationLinkDetails.java
+++ OntoRama/src/ontorama/ontologyConfig/RelationLinkDetails.java
@@ -3,6 +3,7 @@ package ontorama.ontologyConfig;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 
 /**
  * Title:
@@ -17,105 +18,123 @@ import java.awt.Toolkit;
 public class RelationLinkDetails {
 
     /**
-     *
+     * name of link and reversed link
+     * for example: relation id = 0, link name = 'subtype',
+     * reversed link name = 'supertype'
      */
     private String linkName = null;
     private String reversedLinkName = null;
 
     /**
-     *
+     * symbols for link and reversed link
      */
     private String linkSymbol = null;
     private String reversedLinkSymbol = null;
 
     /**
-     *
+     * display color for this relation
      */
     private Color displayColor;
 
     /**
-     *
+     * display symbol for this relation
      */
     private String displaySymbol = null;
-    private String reversedLinkDisplaySymbol = null;
 
     /**
-     *
+     * display image for this relation.
+     * Display image may be built using displayColor and
+     * displaySymbol or supplied by user in congiguration
+     * xml file by specifying URI to the image.
      */
     private Image displayImage;
 
     /**
-     *
+     * Create an object RelationLinkDetails for specified relation link name.
+     * Setters can be used to specify all other relation link properties,
+     * such as: link symbol, reversed link symbol, display color,
+     * display symbol and display image.
      */
     public RelationLinkDetails(String linkName) {
         this.linkName = linkName;
     }
 
     /**
-     *
+     * Get relation link name
+     * @return  linkName
      */
     public String getLinkName () {
         return this.linkName;
     }
 
     /**
-     *
+     * Set relation link symbol
+     * @param   linkSymbol
      */
     public void setLinkSymbol (String linkSymbol) {
         this.linkSymbol = linkSymbol;
     }
 
     /**
-     *
+     * Get relation link symbol
+     * @return  linkSymbol
      */
     public String getLinkSymbol () {
         return this.linkSymbol;
     }
 
     /**
-     *
+     * Get Reversed Link Name
+     * @return  reversedLinkName
      */
     public String getReversedLinkName () {
         return this.reversedLinkName;
     }
 
     /**
-     *
+     * Set reversedLinkName
+     * @param   reversedLinkName
      */
     public void setReversedLinkName(String reversedLinkName) {
         this.reversedLinkName = reversedLinkName;
     }
 
     /**
-     *
+     * Get reversedLinkSymbol
+     * @return  reversedLinkSymbol
      */
     public String getReversedLinkSymbol () {
         return this.reversedLinkSymbol;
     }
 
     /**
-     *
+     * Set reversedLinkSymbol
+     * @param reversedLinkSymbol
      */
     public void setReversedLinkSymbol (String reversedLinkSymbol) {
         this.reversedLinkSymbol = reversedLinkSymbol;
     }
 
     /**
-     *
+     * Set display color
+     * @param string colorStr, colorStr should be a valid color that can be decoded using Color.decode
+     * @see java.awt.Color
      */
     public void setDisplayColor (String colorStr) {
         this.displayColor = Color.decode(colorStr);
     }
 
     /**
-     *
+     * Get display color
+     * @return Color displayColor
      */
     public Color getDisplayColor () {
         return this.displayColor;
     }
 
     /**
-     *
+     * Set display symbol and make an image for it
+     * @param String displaySymbol
      */
     public void setDisplaySymbol (String displaySymbol) {
         this.displaySymbol = displaySymbol;
@@ -123,7 +142,8 @@ public class RelationLinkDetails {
     }
 
     /**
-     *
+     * Get displaySymbol
+     * @return  displaySymbol string
      */
     public String getDisplaySymbol () {
         return this.displaySymbol;
@@ -131,6 +151,8 @@ public class RelationLinkDetails {
 
 
     /**
+     * Set displayImage
+     * @param   imageStr
      * @todo    look into parameter for getImage - use relative paths
      */
     public void setDisplayImage (String imageStr) {
@@ -145,9 +167,19 @@ public class RelationLinkDetails {
     }
 
     /**
-     *
+     * Get display image
+     * @return  Image displayImage
      */
      public Image getDisplayImage () {
         return this.displayImage;
+     }
+
+     /**
+      * Get displayIcon
+      * @return ImageIcon displayIcon
+      */
+     public ImageIcon getDisplayIcon () {
+        ImageIcon icon = new ImageIcon(this.displayImage);
+        return icon;
      }
 }

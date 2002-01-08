12779ccbf8a5ede41dd9d262aae0a1c4c3e26f31
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-01-08 01:09:21 +0000

    Clean Up. In GraphNode: removed children and parents lists and corresponding methods.

--- OntoRama/src/ontorama/model/TestGraphNode.java
+++ OntoRama/src/ontorama/model/TestGraphNode.java
@@ -21,32 +21,32 @@ public class TestGraphNode {
     this.nodeName = nodeName;
     this.childrenList = childrenList;
 
-    node = new GraphNode (nodeName);
-
-    printMessage ("Testing GraphNode");
-    printMessage ("testing getName...");
-    getNameTest();
-
-    GraphNode childNode1 = new GraphNode((String) childrenList.get(0));
-    GraphNode childNode2 = new GraphNode ((String) childrenList.get(1));
-    node.addChild(childNode1);
-    node.addChild(childNode2);
-
-    printMessage("testing getChildrenIterator....");
-    getChildrenIteratorTest();
-
-    printMessage("testing getChildrenList....");
-    getChildrenListTest();
-
-
-    printMessage("---not sure how to test addChild...");
-    printMessage("---not sure how to test addClone...");
-    printMessage("---not sure how to test addObserver...");
-    printMessage("---not sure how to test calculateDepths...");
-    printMessage("---not sure how to test getClones...");
-    printMessage("---not sure how to test getDepth...");
-
-    printMessage ("all tests were successfull");
+//    node = new GraphNode (nodeName);
+//
+//    printMessage ("Testing GraphNode");
+//    printMessage ("testing getName...");
+//    getNameTest();
+//
+//    GraphNode childNode1 = new GraphNode((String) childrenList.get(0));
+//    GraphNode childNode2 = new GraphNode ((String) childrenList.get(1));
+//    node.addChild(childNode1);
+//    node.addChild(childNode2);
+//
+//    printMessage("testing getChildrenIterator....");
+//    getChildrenIteratorTest();
+//
+//    printMessage("testing getChildrenList....");
+//    getChildrenListTest();
+//
+//
+//    printMessage("---not sure how to test addChild...");
+//    printMessage("---not sure how to test addClone...");
+//    printMessage("---not sure how to test addObserver...");
+//    printMessage("---not sure how to test calculateDepths...");
+//    printMessage("---not sure how to test getClones...");
+//    printMessage("---not sure how to test getDepth...");
+//
+//    printMessage ("all tests were successfull");
   }
 
   private void getNameTest () {
@@ -56,35 +56,35 @@ public class TestGraphNode {
   }
 
   private void getChildrenIteratorTest () {
-    Iterator it = node.getChildrenIterator();
-    int count = 0;
-    while (it.hasNext()) {
-      GraphNode child = (GraphNode) it.next();
-      String testName = (String) childrenList.get(count);
-      String childName = child.getName();
-      if ( ! childName.equals(testName) ) {
-        error ("getChildrenIterator failed, returned " + childName + " instead of " + testName);
-      }
-      //printMessage("count = " + count + ", childName = " + childName + ", testName = " + testName);
-      count++;
-    }
+//    Iterator it = node.getChildrenIterator();
+//    int count = 0;
+//    while (it.hasNext()) {
+//      GraphNode child = (GraphNode) it.next();
+//      String testName = (String) childrenList.get(count);
+//      String childName = child.getName();
+//      if ( ! childName.equals(testName) ) {
+//        error ("getChildrenIterator failed, returned " + childName + " instead of " + testName);
+//      }
+//      //printMessage("count = " + count + ", childName = " + childName + ", testName = " + testName);
+//      count++;
+//    }
   }
 
   private void getChildrenListTest () {
-    List testList = (List) node.getChildrenList();
-    List childList = (List) this.childrenList;
-    int size = childList.size();
-    int count = 0;
-    while (count < size) {
-      String childName = (String) childList.get(count);
-      GraphNode testNode = (GraphNode) testList.get(count);
-      String testNodeName = testNode.getName();
-      if ( ! (testNodeName.equals(childName) )) {
-        error ("getChildrenList failed, returned " + childName + " instead of " + testNodeName);
-      }
-      //printMessage("count = " + count + ", child = " + childName + ", testName = " + testNodeName);
-      count++;
-    }
+//    List testList = (List) node.getChildrenList();
+//    List childList = (List) this.childrenList;
+//    int size = childList.size();
+//    int count = 0;
+//    while (count < size) {
+//      String childName = (String) childList.get(count);
+//      GraphNode testNode = (GraphNode) testList.get(count);
+//      String testNodeName = testNode.getName();
+//      if ( ! (testNodeName.equals(childName) )) {
+//        error ("getChildrenList failed, returned " + childName + " instead of " + testNodeName);
+//      }
+//      //printMessage("count = " + count + ", child = " + childName + ", testName = " + testNodeName);
+//      count++;
+//    }
 
   }
 
@@ -99,13 +99,13 @@ public class TestGraphNode {
 
   public static void main(String[] args)  {
 
-    String nodeName = "testNode";
-    LinkedList children = new LinkedList();
-    children.add("child1");
-    children.add("child2");
-
-    TestGraphNode test = new TestGraphNode(nodeName, children);
-
+//    String nodeName = "testNode";
+//    LinkedList children = new LinkedList();
+//    children.add("child1");
+//    children.add("child2");
+//
+//    TestGraphNode test = new TestGraphNode(nodeName, children);
+//
   }
 
 }

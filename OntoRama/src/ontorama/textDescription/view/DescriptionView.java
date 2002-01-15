268b3592acc82955c0a1d14c07280f3aa301eba9
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-01-15 05:26:25 +0000

    Fixed a bug: getting null pointer exception when click on clones. Cause: cloned nodes didn't get any properties copied over from original node.
    DesctiptionView got BoxLayout instead of GridLayout.
    
    Creating ClonesPanel and NodePropertiesPanel

--- OntoRama/src/ontorama/textDescription/view/DescriptionView.java
+++ OntoRama/src/ontorama/textDescription/view/DescriptionView.java
@@ -8,8 +8,20 @@ import java.util.Enumeration;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.border.*;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+
 import java.awt.LayoutManager;
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Component;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+
 
 import ontorama.model.*;
 import ontorama.ontologyConfig.*;
@@ -33,6 +45,17 @@ public class DescriptionView extends JPanel implements NodeObserver  {
   private Hashtable labels = new Hashtable();
 
   /**
+   * Keys - name of property
+   * Values - panel
+   */
+  private Hashtable nodePropertiesPanels = new Hashtable();
+
+  /**
+   *
+   */
+   private ClonesPanel clonesPanel;
+
+  /**
    * @todo  think of a way to not hardcode clones
    */
   String clonesLabelName = "Clones";
@@ -48,35 +71,101 @@ public class DescriptionView extends JPanel implements NodeObserver  {
     }
 
     // set up hashtable of labels
-    initLabels();
+    //initLabels();
+    initPropertiesPanels();
 
     // set layout manager
-    this.setLayout(new GridLayout(labels.size(),1));
+    this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 
     // add labels to the view
-    Enumeration labelsEnum = labels.keys();
-    while (labelsEnum.hasMoreElements()) {
-      JLabel curLabel = (JLabel) labelsEnum.nextElement();
-      this.add(curLabel);
-      this.add( (JLabel) labels.get(curLabel));
+//    Enumeration labelsEnum = labels.keys();
+//    int maxLeftLabelWidth = getMaxLabelWidth();
+//    int minPadding = 15;
+//    while (labelsEnum.hasMoreElements()) {
+//      JLabel curLabel = (JLabel) labelsEnum.nextElement();
+//      JLabel curValueLabel = (JLabel) labels.get(curLabel);
+//      // create a panel for a pair
+//      JPanel curPanel = new JPanel();
+//      // create and set layout
+//      LayoutManager curLayout = new BoxLayout(curPanel,BoxLayout.X_AXIS);
+//      curPanel.setLayout(curLayout);
+//      curPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+//      // add first label
+//      curPanel.add(curLabel);
+//      // work out size of rigid area and set it
+//      int curRigitAreaWidth = maxLeftLabelWidth - getLabelWidth(curLabel) + minPadding;
+//      Dimension d = new Dimension(curRigitAreaWidth,0);
+//      curPanel.add(Box.createRigidArea(d));
+//      // add second label
+//      curPanel.add(curValueLabel);
+//      // finally, add created panel to the main layout
+//      this.add(curPanel);
+//    }
+    Enumeration propPanelsEnum = nodePropertiesPanels.keys();
+    while (propPanelsEnum.hasMoreElements()) {
+        String propName = (String) propPanelsEnum.nextElement();
+        JPanel propPanel = (JPanel) nodePropertiesPanels.get(propName);
+        this.add(propPanel);
     }
-
+    this.add(clonesPanel);
   }
 
   /**
    * Initialise labels that describe concept/node properties
    * @todo  think of a way to not hardcode clones
    */
-  private void initLabels() {
-    Enumeration e = OntoramaConfig.getConceptPropertiesTable().keys();
+//  private void initLabels() {
+//    Enumeration e = OntoramaConfig.getConceptPropertiesTable().keys();
+//    while (e.hasMoreElements()) {
+//      String detailsName = (String) e.nextElement();
+//      JLabel label = new JLabel(detailsName);
+//      JLabel valuesLabel = new JLabel("");
+//      this.labels.put(label, valuesLabel);
+//    }
+//    // add clones
+//    JLabel clonesLabel = new JLabel(clonesLabelName);
+//    JLabel clonesValuesLabel = new JLabel("");
+//    this.labels.put(clonesLabel,clonesValuesLabel);
+//  }
+
+  /**
+   *
+   */
+   private void initPropertiesPanels() {
+        Enumeration e = OntoramaConfig.getConceptPropertiesTable().keys();
+        while (e.hasMoreElements()) {
+            String propName = (String) e.nextElement();
+            NodePropertiesPanel propPanel = new NodePropertiesPanel(propName,new LinkedList());
+            nodePropertiesPanels.put(propName,propPanel);
+        }
+        this.clonesPanel = new ClonesPanel();
+   }
+
+  /**
+   * find max label width
+   */
+   private int getMaxLabelWidth() {
+    Enumeration e = labels.keys();
+    int length = 0;
     while (e.hasMoreElements()) {
-      String detailsName = (String) e.nextElement();
-      JLabel label = new JLabel(detailsName);
-      this.labels.put(label, new JLabel(""));
+      JLabel curLabel = (JLabel) e.nextElement();
+      int width = getLabelWidth(curLabel);
+      if (width > 0) {
+        length = width;
+      }
     }
-    // add clones
-    this.labels.put(new JLabel(clonesLabelName),new JLabel());
-  }
+    return length;
+   }
+
+   /**
+    * get label width
+    */
+    private int getLabelWidth (JLabel label) {
+      Font font = label.getFont();
+      FontMetrics fontMetrics = label.getFontMetrics(font);
+      int width = fontMetrics.stringWidth(label.getText());
+      return width;
+    }
 
   /**
    * Method called to update observer.
@@ -85,32 +174,38 @@ public class DescriptionView extends JPanel implements NodeObserver  {
    */
   public void update( Object observer, Object observable ) {
     GraphNode node = (GraphNode) observable;
-    //System.out.println(".......method update, node = " + node.getName());
-    Enumeration e = this.labels.keys();
+    System.out.println(".......method update, node = " + node.getName());
+    Enumeration e = this.nodePropertiesPanels.keys();
     while (e.hasMoreElements()) {
-      JLabel keyLabel = (JLabel) e.nextElement();
-      JLabel valueLabel = (JLabel) this.labels.get(keyLabel);
+        String propertyName = (String) e.nextElement();
+      //JLabel keyLabel = (JLabel) e.nextElement();
+      //JLabel valueLabel = (JLabel) this.labels.get(keyLabel);
 
-      if (keyLabel.getText().equals(clonesLabelName)) {
-        Iterator clonesIterator = node.getClones();
-        String clonesString = "";
-        while (clonesIterator.hasNext()) {
-          GraphNode clone = (GraphNode) clonesIterator.next();
-          clonesString = clonesString + clone.getName() + " ";
-        }
-        valueLabel.setText(clonesString);
-        continue;
-      }
+//      if (keyLabel.getText().equals(clonesLabelName)) {
+//        Iterator clonesIterator = node.getClones();
+//        String clonesString = "";
+//        while (clonesIterator.hasNext()) {
+//          GraphNode clone = (GraphNode) clonesIterator.next();
+//          clonesString = clonesString + clone.getName() + " ";
+//        }
+//        //valueLabel.setText(clonesString);
+//        continue;
+//      }
 
-      String propertyName = keyLabel.getText();
-      String propertyValue = "";
-      Iterator propertyValueIterator = node.getProperty(propertyName).iterator();
-      while (propertyValueIterator.hasNext()) {
-        propertyValue = propertyValue + (String) propertyValueIterator.next() + " ";
-      }
-      valueLabel.setText( propertyValue);
+      //String propertyName = keyLabel.getText();
+      NodePropertiesPanel propPanel = (NodePropertiesPanel) nodePropertiesPanels.get(propertyName);
+      propPanel.update(node.getProperty(propertyName));
+
+//      String propertyValue = "";
+//      Iterator propertyValueIterator = node.getProperty(propertyName).iterator();
+//      while (propertyValueIterator.hasNext()) {
+//        propertyValue = propertyValue + (String) propertyValueIterator.next() + " ";
+//      }
+//      valueLabel.setText( propertyValue);
+
     }
-
+    // deal with clones
+    clonesPanel.update(node.getClones());
   }
 
 }

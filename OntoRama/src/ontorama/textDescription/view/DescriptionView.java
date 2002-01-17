56fad59f56a39815861947fa842654f237d135fe
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-01-17 05:17:00 +0000

    Description View is now a scroll panel, so far without scroll bars. Minor changes in split Panel resizing

--- OntoRama/src/ontorama/textDescription/view/DescriptionView.java
+++ OntoRama/src/ontorama/textDescription/view/DescriptionView.java
@@ -12,6 +12,7 @@ import javax.swing.BoxLayout;
 import javax.swing.border.*;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.JScrollPane;
 
 import java.awt.LayoutManager;
 import java.awt.BorderLayout;
@@ -78,29 +79,6 @@ public class DescriptionView extends JPanel implements NodeObserver  {
     this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 
     // add labels to the view
-//    Enumeration labelsEnum = labels.keys();
-//    int maxLeftLabelWidth = getMaxLabelWidth();
-//    int minPadding = 15;
-//    while (labelsEnum.hasMoreElements()) {
-//      JLabel curLabel = (JLabel) labelsEnum.nextElement();
-//      JLabel curValueLabel = (JLabel) labels.get(curLabel);
-//      // create a panel for a pair
-//      JPanel curPanel = new JPanel();
-//      // create and set layout
-//      LayoutManager curLayout = new BoxLayout(curPanel,BoxLayout.X_AXIS);
-//      curPanel.setLayout(curLayout);
-//      curPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-//      // add first label
-//      curPanel.add(curLabel);
-//      // work out size of rigid area and set it
-//      int curRigitAreaWidth = maxLeftLabelWidth - getLabelWidth(curLabel) + minPadding;
-//      Dimension d = new Dimension(curRigitAreaWidth,0);
-//      curPanel.add(Box.createRigidArea(d));
-//      // add second label
-//      curPanel.add(curValueLabel);
-//      // finally, add created panel to the main layout
-//      this.add(curPanel);
-//    }
     Enumeration propPanelsEnum = nodePropertiesPanels.keys();
     while (propPanelsEnum.hasMoreElements()) {
         String propName = (String) propPanelsEnum.nextElement();
@@ -109,24 +87,6 @@ public class DescriptionView extends JPanel implements NodeObserver  {
     }
     this.add(clonesPanel);
   }
-
-  /**
-   * Initialise labels that describe concept/node properties
-   * @todo  think of a way to not hardcode clones
-   */
-//  private void initLabels() {
-//    Enumeration e = OntoramaConfig.getConceptPropertiesTable().keys();
-//    while (e.hasMoreElements()) {
-//      String detailsName = (String) e.nextElement();
-//      JLabel label = new JLabel(detailsName);
-//      JLabel valuesLabel = new JLabel("");
-//      this.labels.put(label, valuesLabel);
-//    }
-//    // add clones
-//    JLabel clonesLabel = new JLabel(clonesLabelName);
-//    JLabel clonesValuesLabel = new JLabel("");
-//    this.labels.put(clonesLabel,clonesValuesLabel);
-//  }
 
   /**
    *
@@ -174,38 +134,15 @@ public class DescriptionView extends JPanel implements NodeObserver  {
    */
   public void update( Object observer, Object observable ) {
     GraphNode node = (GraphNode) observable;
-    System.out.println(".......method update, node = " + node.getName());
+    //System.out.println(".......method update, node = " + node.getName());
     Enumeration e = this.nodePropertiesPanels.keys();
     while (e.hasMoreElements()) {
-        String propertyName = (String) e.nextElement();
-      //JLabel keyLabel = (JLabel) e.nextElement();
-      //JLabel valueLabel = (JLabel) this.labels.get(keyLabel);
-
-//      if (keyLabel.getText().equals(clonesLabelName)) {
-//        Iterator clonesIterator = node.getClones();
-//        String clonesString = "";
-//        while (clonesIterator.hasNext()) {
-//          GraphNode clone = (GraphNode) clonesIterator.next();
-//          clonesString = clonesString + clone.getName() + " ";
-//        }
-//        //valueLabel.setText(clonesString);
-//        continue;
-//      }
-
-      //String propertyName = keyLabel.getText();
+      String propertyName = (String) e.nextElement();
       NodePropertiesPanel propPanel = (NodePropertiesPanel) nodePropertiesPanels.get(propertyName);
       propPanel.update(node.getProperty(propertyName));
-
-//      String propertyValue = "";
-//      Iterator propertyValueIterator = node.getProperty(propertyName).iterator();
-//      while (propertyValueIterator.hasNext()) {
-//        propertyValue = propertyValue + (String) propertyValueIterator.next() + " ";
-//      }
-//      valueLabel.setText( propertyValue);
 
     }
     // deal with clones
     clonesPanel.update(node.getClones());
   }
-
 }

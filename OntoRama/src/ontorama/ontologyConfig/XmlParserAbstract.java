ddb284166a8302c96c353d6adb2a82d6c473e8bf
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-02-07 04:40:55 +0000

    First Import. Abstract xml parser that will hold common methods used by other parsers

---
+++ OntoRama/src/ontorama/ontologyConfig/XmlParserAbstract.java
@@ -0,0 +1,32 @@
+package ontorama.ontologyConfig;
+
+import org.jdom.*;
+import org.jdom.input.*;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) DSTC 2002</p>
+ * <p>Company: DSTc</p>
+ * @version 1.0
+ */
+
+public class XmlParserAbstract {
+
+    public XmlParserAbstract() {
+    }
+
+    /**
+     *
+     */
+    public void checkCompulsoryAttr (Attribute attr, String attrName, String elementName)
+                                    throws ConfigParserException {
+        if ( attr == null) {
+            throw new ConfigParserException("Missing compulsory Attribute '" + attrName + "' in Element '" + elementName + "'");
+        }
+        if ( attr.getValue().trim().equals("")) {
+            throw new ConfigParserException("Attribute '" + attrName + "' in Element '" + elementName + "' can't be empty");
+        }
+    }
+
+}

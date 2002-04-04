12c08c78722ae58ac107bc81ddef7fec5ec70e70
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-04-04 04:35:32 +0000

    minor changes

--- OntoRama/src/ontorama/view/AboutOntoRamaDialog.java
+++ OntoRama/src/ontorama/view/AboutOntoRamaDialog.java
@@ -13,6 +13,7 @@ import javax.swing.JDialog;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.Icon;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.BoxLayout;
@@ -27,9 +28,19 @@ public class AboutOntoRamaDialog extends JDialog {
     //this.owner = owner;
     System.out.println("AboutOntoRamaDialog");
 
+    ImageMapping.loadImages();
+
     JLabel label1 = new JLabel ("OntoRama");
     JLabel label2 = new JLabel ("Version: 0.0");
-    JLabel label3 = new JLabel ("Brought to you by DSTC and KVO       ");
+
+    //JPanel panel3 = new JPanel ();
+    //panel3.add(ImageMapping.dstcLogoImage.getImage());
+
+    JLabel label3 = new JLabel ("Brought to you by      ");
+    JLabel label4 = new JLabel ("DSTC ", (Icon) ImageMapping.dstcLogoImage, JLabel.CENTER);
+    JLabel label5 = new JLabel (" and ");
+    JLabel label6 = new JLabel ("KVO ", (Icon) ImageMapping.kvoLogoImage, JLabel.CENTER);
+    //panel3.add(label3);
 
     JButton okButton = new JButton("OK");
     okButton.addActionListener(new ActionListener() {
@@ -50,7 +61,11 @@ public class AboutOntoRamaDialog extends JDialog {
     getContentPane().add(label1);
     getContentPane().add(label2);
     getContentPane().add(label3);
+    getContentPane().add(label4);
+    getContentPane().add(label5);
+    getContentPane().add(label6);
     getContentPane().add(buttonPanel);
+
 
     setTitle("About OntoRama");
     setBackground(Color.white);

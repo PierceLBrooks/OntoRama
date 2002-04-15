98f57b7aa72d69fdcd14e23f51da3e5f42eb7522
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-04-15 05:02:34 +0000

    Adding back (removed by mistake)

---
+++ OntoRama/src/ontorama/model/GraphInterface.java
@@ -0,0 +1,15 @@
+
+package ontorama.model;
+
+/**
+ *
+ */
+
+ public interface GraphInterface {
+  /**
+   * Returns root node of the tree.
+   * The tree is modified from acyclic graph
+   */
+  public GraphNode getRootNode ();
+
+ }

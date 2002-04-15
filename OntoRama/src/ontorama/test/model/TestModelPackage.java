eed03481d1fe0899b28adb87682f06bcd0b5ee11
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-04-15 05:06:44 +0000

    Moving TestGraphNode, TestGraph and TestEdge from package ontorama.test into ontorama.test.model

---
+++ OntoRama/src/ontorama/test/model/TestModelPackage.java
@@ -0,0 +1,28 @@
+package ontorama.test.model;
+
+import junit.framework.*;
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: DSTC</p>
+ * @author nataliya
+ * @version 1.0
+ */
+
+public class TestModelPackage extends TestCase {
+
+    public TestModelPackage(String name) {
+      super(name);
+    }
+
+
+    public static Test suite()
+    {
+       TestSuite suite = new TestSuite( "ontorama.model" );
+       suite.addTest( new TestSuite( TestEdge.class ) );
+       suite.addTest( new TestSuite( TestGraphNode.class ) );
+       suite.addTest( new TestSuite (TestGraph.class) );
+       return suite;
+    }
+}

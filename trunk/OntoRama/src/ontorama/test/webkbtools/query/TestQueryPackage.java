9852c3791201ee7ee2c45e15a69e42a77952b378
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-04-16 04:49:16 +0000

    First import

---
+++ trunk/OntoRama/src/ontorama/test/webkbtools/query/TestQueryPackage.java
@@ -0,0 +1,27 @@
+package ontorama.test.webkbtools.query;
+
+import junit.framework.*;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: DSTC</p>
+ * @author nataliya
+ * @version 1.0
+ */
+
+public class TestQueryPackage extends TestCase {
+
+  public TestQueryPackage(String name) {
+    super (name);
+  }
+
+  public static Test suite() {
+     TestSuite suite = new TestSuite( "ontorama.query" );
+
+     suite.addTest( new TestSuite(TestTypeQueryImplementation.class) );
+
+     return suite;
+  }
+}

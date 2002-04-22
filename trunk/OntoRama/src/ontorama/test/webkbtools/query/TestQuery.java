e79045797eea5d84aad7bd4937c7820ef627f57b
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-04-22 06:09:19 +0000

    TestQuery is complete :)

--- trunk/OntoRama/src/ontorama/test/webkbtools/query/TestQuery.java
+++ trunk/OntoRama/src/ontorama/test/webkbtools/query/TestQuery.java
@@ -71,14 +71,14 @@ public class TestQuery extends TestCase {
   /**
    * test method getRelationLinksCollection
    */
-//  public void testGetRelationLinksCollection () {
-//    assertEquals("relaton links collection for query1 should be empty", 0,
-//                      query1.getRelationLinksCollection().size());
-//
-//    Collection expectedCollection2 = (Collection) expectedRelLinksList2;
-//    assertEquals("relation links collection for query2", expectedCollection2,
-//                      query2.getRelationLinksCollection());
-//  }
+  public void testGetRelationLinksCollection () {
+    assertEquals("relaton links collection for query1 should be empty", 0,
+                      query1.getRelationLinksCollection().size());
+
+    Collection expectedCollection2 = (Collection) expectedRelLinksList2;
+    assertEquals("relation links collection for query2", expectedCollection2,
+                      query2.getRelationLinksCollection());
+  }
 
   /**
    * test method getRelationLinksIterator
@@ -94,9 +94,37 @@ public class TestQuery extends TestCase {
                     true, IteratorUtil.objectIsInIterator(new Integer(2), query2.getRelationLinksIterator()));
     assertEquals("relation links iterator for query2 should not contain integer 3",
                     false, IteratorUtil.objectIsInIterator(new Integer(3), query2.getRelationLinksIterator()));
-
-
   }
 
+  /**
+   *
+   */
+  public void testGetRelationLinksList () {
+    assertEquals("relation links list for query1 should be empty", 0,
+                          query1.getRelationLinksList().size());
+    assertEquals("size of relation links list for query2", expectedRelLinksList2.size(),
+                          query2.getRelationLinksList().size());
+    for (int i = 0; i < expectedRelLinksList2.size(); i++) {
+      Integer int1 = (Integer) expectedRelLinksList2.get(i);
+      Integer int2 = (Integer) query2.getRelationLinksList().get(i);
+      assertEquals("elements of lists in the same position should be the same",
+                          int1, int2);
+    }
+  }
 
+  /**
+   *
+   */
+  public void testSetRelationLinks() {
+    LinkedList testRelationLinksList = new LinkedList();
+    testRelationLinksList.add(new Integer(22));
+    testRelationLinksList.add(new Integer(4));
+
+    query2.setRelationLinks(testRelationLinksList);
+
+    assertEquals(testRelationLinksList.size(), query2.getRelationLinksList().size());
+
+    assertEquals(new Integer(22), query2.getRelationLinksList().get(0));
+    assertEquals(new Integer(4), query2.getRelationLinksList().get(1));
+  }
 }

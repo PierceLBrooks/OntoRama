cff463c3f7f2ea035b6463ed983c62d60c934dc0
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-04-29 03:58:55 +0000

    minor updates

--- OntoRama/src/ontorama/test/webkbtools/query/TestQueryPackage.java
+++ OntoRama/src/ontorama/test/webkbtools/query/TestQueryPackage.java
@@ -2,6 +2,7 @@ package ontorama.test.webkbtools.query;
 
 import junit.framework.*;
 
+import ontorama.OntoramaConfig;
 import ontorama.test.webkbtools.query.parser.rdf.TestRdfDamlParser;
 
 /**
@@ -22,12 +23,14 @@ public class TestQueryPackage extends TestCase {
   public static Test suite() {
      TestSuite suite = new TestSuite( "ontorama.query" );
 
-     suite.addTest( new TestSuite(TestQuery.class) );
-
-     //suite.addTest( new TestSuite(TestTypeQueryImplementation.class) );
-
-     //suite.addTest( new TestSuite(TestQueryEngine.class));
      suite.addTest( new TestSuite(TestRdfDamlParser.class));
+
+//    OntoramaConfig.loadAllConfig("examples/test/data/examplesConfig.xml",
+//                            "ontorama.properties","examples/test/data/config.xml");
+     suite.addTest( new TestSuite(TestTypeQueryImplementation.class) );
+
+     suite.addTest( new TestSuite(TestQuery.class) );
+     suite.addTest( new TestSuite(TestQueryEngine.class));
 
      return suite;
   }

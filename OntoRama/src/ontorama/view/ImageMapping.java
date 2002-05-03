eb0c8e96d96f621129ecf8c646ae371d022e4ef9
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-05-03 03:04:05 +0000

    First import

---
+++ OntoRama/src/ontorama/view/ImageMapping.java
@@ -0,0 +1,38 @@
+package ontorama.view;
+
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+import ontorama.OntoramaConfig;
+
+
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) DSTC 2002</p>
+ * <p>Company: DSTC</p>
+ *  unascribed
+ * @version 1.0
+ * @todo  null pointer exception handling: need some usefull errormessage (if img dir is not found).
+ */
+
+public class ImageMapping {
+  private static String dstcLogoLocation = "img/dstc.gif";
+  private static String kvoLogoLocation = "img/kvo-logo.gif";
+
+  public static ImageIcon dstcLogoImage;
+  public static ImageIcon kvoLogoImage;
+
+  public static void loadImages () {
+    dstcLogoImage = loadImage (dstcLogoLocation);
+    kvoLogoImage = loadImage (kvoLogoLocation);
+  }
+
+  private static ImageIcon loadImage (String relativeUri) {
+    URL url =  OntoramaConfig.getClassLoader().getResource(relativeUri);
+    ImageIcon imageIcon = new ImageIcon(url);
+    return imageIcon;
+  }
+}

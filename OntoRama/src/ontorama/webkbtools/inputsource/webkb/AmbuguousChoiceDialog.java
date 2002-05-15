7efb001a54a18f2b864c245bffe6db05e39cda93
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-05-15 23:49:10 +0000

    Tidying up and documenting

--- OntoRama/src/ontorama/webkbtools/inputsource/webkb/AmbuguousChoiceDialog.java
+++ OntoRama/src/ontorama/webkbtools/inputsource/webkb/AmbuguousChoiceDialog.java
@@ -23,7 +23,8 @@ import java.util.LinkedList;
 import java.util.Iterator;
 import java.util.Collection;
 
-import ontorama.webkbtools.datamodel.*;
+import ontorama.webkbtools.datamodel.OntologyType;
+import ontorama.webkbtools.datamodel.OntologyTypeImplementation;
 import ontorama.webkbtools.util.*;
 
 /**
@@ -37,25 +38,73 @@ import ontorama.webkbtools.util.*;
 
 public class AmbuguousChoiceDialog extends JDialog {
 
+  /**
+   * list to hold objects to choose from
+   * (in our case - ontology types)
+   */
   private List choiceList;
   private int numChoices;
+
+  /**
+   * group of choice buttons
+   */
   private ButtonGroup group;
 
+  /**
+   * remember selected button
+   */
   private JRadioButton selectedButton;
 
+  /**
+   * title string
+   */
+  private String titleStr = "Search Result is ambiguous";
+
+  /**
+   * 'explanation' string that will appear above choices
+   */
+  private String topLabelStr = "Search term is ambiguous...Please choose one of the following ";
+
+  /**
+   * name of property used in tool tips
+   * @todo think of a way not to hard code property name here, because if it is
+   * changed in the config.xml - we simply silently loose this feature. Or think
+   * of a way to inform about this (where catching NoSuchPropertyException)
+   */
+  private String descrPropName = "Description";
+
+  /**
+   * Create a dialog that will display all choices and propmt user
+   * to make a selection.
+   * @param typesList - list holding all alternative ontology types
+   * @param frame - owner frame
+   */
   public AmbuguousChoiceDialog(List typesList, Frame frame) {
-    super(frame, "title here");
+    super(frame);
+    setTitle(titleStr);
 
     this.choiceList = typesList;
     this.numChoices = choiceList.size();
 
     //System.out.println ("choice list = " + choiceList);
     JRootPane rootPane = getRootPane();
-    Container contentPane = rootPane.getContentPane();
+    Container contentPanel = rootPane.getContentPane();
 
     JPanel choicesPanel = buildChoicePanel();
 
-    contentPane.add(choicesPanel);
+    //contentPanel.add(choicesPanel);
+
+    //JPanel mainPanel = new JPanel();
+    contentPanel.setLayout(new BorderLayout());
+    contentPanel.add(choicesPanel, BorderLayout.NORTH);
+
+    JButton okButton = new JButton("OK");
+    okButton.addActionListener(new ActionListener () {
+      public void actionPerformed (ActionEvent ae) {
+        closeDialog();
+      }
+    });
+    contentPanel.add(okButton, BorderLayout.SOUTH);
 
     setModal(true);
     setLocationRelativeTo(frame);
@@ -67,6 +116,12 @@ public class AmbuguousChoiceDialog extends JDialog {
    *
    */
   private JPanel buildChoicePanel () {
+    JPanel choiceButtonsPanel = new JPanel();
+    JLabel label = new JLabel(topLabelStr);
+
+    choiceButtonsPanel.setLayout(new BoxLayout(choiceButtonsPanel, BoxLayout.Y_AXIS));
+    choiceButtonsPanel.add(label);
+
     JRadioButton[] radioButtons = new JRadioButton[choiceList.size()];
     group = new ButtonGroup();
 
@@ -74,6 +129,16 @@ public class AmbuguousChoiceDialog extends JDialog {
       OntologyType curType = (OntologyType) choiceList.get(i);
       //JRadioButton curButton = new JRadioButton( (String) choiceList.get(i));
       JRadioButton curButton = new JRadioButton( curType.getName());
+      try {
+        List descrPropValue = curType.getTypeProperty(descrPropName);
+        if (descrPropValue.size() > 0 ) {
+          curButton.setToolTipText( (String) descrPropValue.get(0));
+        }
+      }
+      catch (NoSuchPropertyException e) {
+        // just do nothing - don't display any tool tips
+        ///todo: should find a way to unobtrusively complain about this
+      }
       radioButtons[i] = curButton;
       if (i == 0) {
         curButton.setSelected(true);
@@ -87,31 +152,9 @@ public class AmbuguousChoiceDialog extends JDialog {
         }
       });
       group.add(curButton);
+      choiceButtonsPanel.add(curButton);
     }
-
-    JPanel box = new JPanel();
-    JLabel label = new JLabel("Search term is ambuguous...Please choose one of the following ");
-
-    box.setLayout(new BoxLayout(box, BoxLayout.Y_AXIS));
-    box.add(label);
-
-    for (int i = 0; i < numChoices; i++) {
-        box.add(radioButtons[i]);
-    }
-
-    JPanel pane = new JPanel();
-    pane.setLayout(new BorderLayout());
-    pane.add(box, BorderLayout.NORTH);
-
-    JButton okButton = new JButton("OK");
-    okButton.addActionListener(new ActionListener () {
-      public void actionPerformed (ActionEvent ae) {
-        closeDialog();
-      }
-    });
-    pane.add(okButton, BorderLayout.SOUTH);
-
-    return pane;
+    return choiceButtonsPanel;
   }
 
   /**

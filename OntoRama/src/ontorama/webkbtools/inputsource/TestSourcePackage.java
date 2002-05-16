7f30eb4e5210ac2570835502f19ee09ddec0259e
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-05-16 01:35:56 +0000

    First Import

---
+++ OntoRama/src/ontorama/webkbtools/inputsource/TestSourcePackage.java
@@ -0,0 +1,30 @@
+package ontorama.webkbtools.inputsource;
+
+import junit.framework.*;
+
+import ontorama.webkbtools.inputsource.webkb.TestWebkbQueryStringConstructor;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: DSTC</p>
+ * @author nataliya
+ * @version 1.0
+ */
+
+public class TestSourcePackage extends TestCase {
+
+  public TestSourcePackage(String name) {
+    super(name);
+  }
+
+  public static Test suite() {
+     TestSuite suite = new TestSuite( "ontorama.inputsource" );
+
+     suite.addTest( new TestSuite(TestWebKB2Source.class));
+     suite.addTest( new TestSuite(TestWebkbQueryStringConstructor.class));
+
+     return suite;
+  }
+}

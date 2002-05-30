4cd304dd2628138c5aee5b256e19c72f1d821985
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-05-30 03:51:42 +0000

    Refactoring the way GUI components are working together in order to simplify the code. Introducing AbstractAction implementation for various interface actions

---
+++ trunk/OntoRama/src/ontorama/view/action/AboutOntoRamaAction.java
@@ -0,0 +1,45 @@
+package ontorama.view.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.Frame;
+import javax.swing.*;
+
+import ontorama.view.AboutOntoRamaDialog;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: DSTC</p>
+ * @author nataliya
+ * @version 1.0
+ */
+
+public class AboutOntoRamaAction extends AbstractAction {
+
+  private static final String ACTION_COMMAND_KEY_COPY = "about-ontorama-command";
+  private static final String NAME_COPY = "About OntoRama";
+  private static final String SHORT_DESCRIPTION_COPY = "About OntoRama Application";
+  private static final String LONG_DESCRIPTION_COPY = "Find out more about OntoRama program";
+
+  /**
+   *
+   */
+  public AboutOntoRamaAction() {
+    putValue(Action.NAME, NAME_COPY);
+    putValue(Action.SHORT_DESCRIPTION, SHORT_DESCRIPTION_COPY);
+    putValue(Action.LONG_DESCRIPTION, LONG_DESCRIPTION_COPY);
+    putValue(Action.ACTION_COMMAND_KEY, ACTION_COMMAND_KEY_COPY);
+  }
+
+  public void actionPerformed(ActionEvent parm1) {
+      System.out.println("___action: about ontorama");
+      Frame[] frames = ontorama.view.OntoRamaApp.getFrames();
+      if (frames.length > 0) {
+        AboutOntoRamaDialog dialog = new AboutOntoRamaDialog( frames[0]);
+      }
+      else {
+        AboutOntoRamaDialog dialog = new AboutOntoRamaDialog( null);
+      }
+  }
+}
---
+++ trunk/OntoRama/src/ontorama/view/action/BackHistoryAction.java
@@ -0,0 +1,53 @@
+package ontorama.view.action;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+import ontorama.view.HistoryMenu;
+
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: DSTC</p>
+ * @author nataliya
+ * @version 1.0
+ */
+
+public class BackHistoryAction extends AbstractAction {
+
+  private static final String ACTION_COMMAND_KEY_COPY = "back-history-command";
+  private static final String NAME_COPY = "Back";
+  private static final String SHORT_DESCRIPTION_COPY = "Back to previous ontology";
+  private static final String LONG_DESCRIPTION_COPY = "Back to previous ontology";
+  //private static final String ACCELERATOR_KEY=KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, ActionEvent.ALT_MASK);
+//  private static final String ACCELERATOR_KEY="ALT+Left";
+
+
+  /**
+   *
+   */
+  public BackHistoryAction() {
+    putValue(Action.NAME, NAME_COPY);
+    putValue(Action.SHORT_DESCRIPTION, SHORT_DESCRIPTION_COPY);
+    putValue(Action.LONG_DESCRIPTION, LONG_DESCRIPTION_COPY);
+//    putValue(Action.ACCELERATOR_KEY, ACCELERATOR_KEY);
+    putValue(Action.ACTION_COMMAND_KEY, ACTION_COMMAND_KEY_COPY);
+  }
+
+  /**
+   *
+   */
+  public void actionPerformed(ActionEvent parm1) {
+//    int indexOfCur = HistoryMenu.getIndexOfSelectedHistoryMenuItem();
+//    int backInd = indexOfCur - 1;
+//    JCheckBoxMenuItem backItem = HistoryMenu.getMenuItem(backInd);
+    System.out.println("___action: back");
+    //HistoryMenu.displayHistoryItem(backItem);
+  }
+
+
+
+}

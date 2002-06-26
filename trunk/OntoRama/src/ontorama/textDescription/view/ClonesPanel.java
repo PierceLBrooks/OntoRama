0c1f403d5526e0826b6e71bf3edff833d36d99f7
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-06-26 06:48:07 +0000

    Introducing AbstractMultiValuePanel that can be a base class for ClonesPanel and Panel that will display node parents in webkb ontology. The reason for this is that Clones Panel and Parents Panel will have the same layout and general setup except for the buttons/components actions. So far, this introduced a bug in clones panel: can't browse to the clone. have to figure out if this was a good idea in the first place.

--- trunk/OntoRama/src/ontorama/textDescription/view/ClonesPanel.java
+++ trunk/OntoRama/src/ontorama/textDescription/view/ClonesPanel.java
@@ -6,22 +6,10 @@ import java.util.LinkedList;
 import java.util.Hashtable;
 import java.util.Enumeration;
 
-import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import javax.swing.BoxLayout;
-import javax.swing.border.*;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
+import javax.swing.JComponent;
 
-import java.awt.LayoutManager;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.Component;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -40,91 +28,23 @@ import ontorama.util.event.ViewEventListener;
  * @author
  * @version 1.0
  */
-public class ClonesPanel extends JPanel {
-    String clonesNameString = "Clones";
-    Iterator clones;
-    JLabel clonesNameLabel = new JLabel();
-    JPanel clonesValuePanel = new JPanel();
-    int minPadding = 40;
-
-    Hashtable buttonCloneMapping = new Hashtable();
-
-    private ViewEventListener viewListener;
-
-    public ClonesPanel (ViewEventListener viewListener) {
-        this.viewListener = viewListener;
-        //this.clones = clones;
-
-        initLabels();
-
-        // debug
-        //this.setBackground(Color.blue);
-        //clonesValuePanel.setBackground(Color.pink);
-        // end of debug
-
-        // create and set layout
-        LayoutManager curLayout = new BoxLayout(this,BoxLayout.X_AXIS);
-        this.setLayout(curLayout);
-        this.setAlignmentX(Component.LEFT_ALIGNMENT);
-        // add first label
-        this.add(clonesNameLabel);
-
-        // work out size of rigid area and set it
-        //int curRigitAreaWidth = maxLeftLabelWidth - getLabelWidth(propNameLabel) + this.minPadding;
-        int curRigitAreaWidth = this.minPadding;
-        Dimension d = new Dimension(curRigitAreaWidth,0);
-        this.add(Box.createRigidArea(d));
-
-        // add second panel
-        clonesValuePanel.setLayout(new BoxLayout(clonesValuePanel, BoxLayout.X_AXIS));
-        clonesValuePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        this.add(clonesValuePanel);
+public class ClonesPanel extends AbstractMultiValuesPanel {
+	
+    public ClonesPanel (String clonesPropName, ViewEventListener viewListener) {
+    	super(clonesPropName, viewListener);
     }
 
-    public JPanel getPanel() {
-        return this;
-    }
-
-    private void initLabels() {
-        clonesNameLabel.setText(this.clonesNameString);
-    }
-
-//    public void setNameLabelSize (Dimension d) {
-//      this.clonesNameLabel.setPreferredSize(d);
-//      this.clonesNameLabel.setMinimumSize(d);
-//    }
-
-    public void update (Iterator clonesIterator) {
-        //System.out.println("ClonesPanel, method update");
-        this.clones = clonesIterator;
-        clonesValuePanel.removeAll();
-        // need updateUI, otherwise it seems that when a user clicks
-        // on a clone button and clone is focused - we don't get a 'clone
-        // button'
-        clonesValuePanel.updateUI();
-        this.buttonCloneMapping = new Hashtable();
-        while (clonesIterator.hasNext()) {
-          GraphNode clone = (GraphNode) clonesIterator.next();
-          //System.out.println("clone = " + clone.getName());
+   private JComponent createPropertyComponent (GraphNode graphNode) {
           JButton button = new JButton();
-          button.setText(clone.getName());
-          buttonCloneMapping.put(button,clone);
+          button.setText(graphNode.getName());
+          _componentToPropValueMapping.put(button, graphNode);
           button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                GraphNode node = (GraphNode) buttonCloneMapping.get(e.getSource());
-                //System.out.println("CLONE button is sending event ViewEventListener.MOUSE_SINGLECLICK for node " + node);
-                viewListener.notifyChange(node, ViewEventListener.MOUSE_SINGLECLICK);
+                GraphNode node = (GraphNode) _componentToPropValueMapping.get(e.getSource());
+                _viewListener.notifyChange(node, ViewEventListener.MOUSE_SINGLECLICK);
             }
           });
-          clonesValuePanel.add(button);
-        }
-        clonesValuePanel.update(clonesValuePanel.getGraphics());
-        //clonesValuePanel.repaint();
-        //this.repaint();
-    }
-
-    public void clear () {
-      clonesValuePanel.removeAll();
+          return (JComponent) button;
     }
 
 }
--- trunk/OntoRama/src/ontorama/textDescription/view/DescriptionView.java
+++ trunk/OntoRama/src/ontorama/textDescription/view/DescriptionView.java
@@ -24,7 +24,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 
-
 import ontorama.model.*;
 import ontorama.ontologyConfig.*;
 import ontorama.OntoramaConfig;
@@ -45,195 +44,233 @@ import ontorama.util.event.ViewEventObserver;
 
 public class DescriptionView extends JPanel implements ViewEventObserver {
 
-  /**
-   * this hashtable will hold labels of concept property names as keys
-   * and labels of corresponding values for a node as values
-   */
-  //private Hashtable labels = new Hashtable();
+	/**
+	 * this hashtable will hold labels of concept property names as keys
+	 * and labels of corresponding values for a node as values
+	 */
+	//private Hashtable labels = new Hashtable();
 
-  /**
-   * Keys - name of property
-   * Values - panel
-   */
-  private Hashtable nodePropertiesPanels = new Hashtable();
+	/**
+	 * Keys - name of property
+	 * Values - panel
+	 */
+	private Hashtable nodePropertiesPanels = new Hashtable();
 
-  /**
-   *
-   */
-   private ClonesPanel clonesPanel;
+	/**
+	 *
+	 */
+	private ClonesPanel clonesPanel;
 
-  /**
-   * @todo  think of a way to not hardcode clones
-   */
-  String clonesLabelName = "Clones";
+	/**
+	 * @todo  think of a way to not hardcode clones
+	 */
+	String clonesLabelName = "Clones";
+	
+	/**
+	 * @todo  think of a way to not hardcode _fullUrlPropName
+	 */
+	private String _fullUrlPropName = "Full Url ";
+	
+	/**
+	 * 
+	 */
+	private NodePropertiesPanel _fullUrlPanel;
 
-  /**
-   *
-   */
-  private Dimension propertyNameLabelsDimension;
+	/**
+	 *
+	 */
+	private Dimension propertyNameLabelsDimension;
 
-  /**
-   *
-   */
-  private ViewEventListener viewListener;
+	/**
+	 *
+	 */
+	private ViewEventListener viewListener;
 
+	/**
+	 *
+	 */
+	public DescriptionView(ViewEventListener viewListener) {
+		this.viewListener = viewListener;
+		this.viewListener.addObserver(this);
 
-  /**
-   *
-   */
-  public DescriptionView( ViewEventListener viewListener) {
-    this.viewListener = viewListener;
-    this.viewListener.addObserver(this);
+		// set up hashtable of panels
+		//initLabels();
+		initPropertiesPanels();
+		initFullUrlPanel();
+		this.propertyNameLabelsDimension = calcLabelSize();
+		setLabelSizes();
 
-    // set up hashtable of panels
-    //initLabels();
-    initPropertiesPanels();
-    this.propertyNameLabelsDimension = calcLabelSize();
-    setLabelSizes();
+		// set layout manager
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-    // set layout manager
-    this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		// add panels to the view
+		Enumeration propPanelsEnum = nodePropertiesPanels.keys();
+		while (propPanelsEnum.hasMoreElements()) {
+			String propName = (String) propPanelsEnum.nextElement();
+			JPanel propPanel = (JPanel) nodePropertiesPanels.get(propName);
+			this.add(propPanel);
+		}
+		this.add(clonesPanel);
+		this.add(_fullUrlPanel);
+	}
 
-    // add panels to the view
-    Enumeration propPanelsEnum = nodePropertiesPanels.keys();
-    while (propPanelsEnum.hasMoreElements()) {
-        String propName = (String) propPanelsEnum.nextElement();
-        JPanel propPanel = (JPanel) nodePropertiesPanels.get(propName);
-        this.add(propPanel);
-    }
-    this.add(clonesPanel);
-  }
+	/**
+	 *
+	 */
+	public void setGraph(Graph graph) {
 
-  /**
-   *
-   */
-  public void setGraph (Graph graph) {
+	}
 
-  }
+	/**
+	 * initialise concept properties panels
+	 */
+	private void initPropertiesPanels() {
+		Enumeration e = OntoramaConfig.getConceptPropertiesTable().keys();
+		while (e.hasMoreElements()) {
+			String propName = (String) e.nextElement();
+			NodePropertiesPanel propPanel =
+				new NodePropertiesPanel(propName, new LinkedList());
+			nodePropertiesPanels.put(propName, propPanel);
+		}
+		this.clonesPanel = new ClonesPanel("Clones",viewListener);
+	}
 
-  /**
-   *
-   */
-   private void initPropertiesPanels() {
-        Enumeration e = OntoramaConfig.getConceptPropertiesTable().keys();
-        while (e.hasMoreElements()) {
-            String propName = (String) e.nextElement();
-            NodePropertiesPanel propPanel = new NodePropertiesPanel(propName,new LinkedList());
-            nodePropertiesPanels.put(propName,propPanel);
+	/**
+	 * initialise panel for displaying full url 
+	 */
+	private void initFullUrlPanel() {
+		_fullUrlPanel = 
+			new NodePropertiesPanel(_fullUrlPropName, new LinkedList());
+	}
+
+	/**
+	 * find max label width
+	 */
+	private int getMaxLabelWidth() {
+		Iterator it = nodePropertiesPanels.values().iterator();
+		//Enumeration e = labels.keys();
+		int length = 0;
+		while (it.hasNext()) {
+			NodePropertiesPanel curPanel = (NodePropertiesPanel) it.next();
+			JLabel curLabel = curPanel.getPropNameLabel();
+			//JLabel curLabel = (JLabel) e.nextElement();
+			int width = curPanel.getPropNameLabelWidth();
+			if (width > 0) {
+				length = width;
+			}
+		}
+		return length;
+	}
+
+	/**
+	 *
+	 */
+	private void setLabelSizes() {
+		Iterator it = nodePropertiesPanels.values().iterator();
+		while (it.hasNext()) {
+			NodePropertiesPanel curPanel = (NodePropertiesPanel) it.next();
+			JLabel curLabel = curPanel.getPropNameLabel();
+			//Dimension d = curLabel.getSize();
+			//System.out.println("d = " + d + ", width = " + maxSize + ", height = " + curPanel.getPropNameLabelHeight());
+			curLabel.setSize(this.propertyNameLabelsDimension);
+			curLabel.setMinimumSize(this.propertyNameLabelsDimension);
+			curLabel.setMaximumSize(this.propertyNameLabelsDimension);
+			curLabel.setPreferredSize(this.propertyNameLabelsDimension);
+		}
+		//this.clonesPanel.setNameLabelSize(this.propertyNameLabelsDimension);
+	}
+
+	/**
+	 *
+	 */
+	private Dimension calcLabelSize() {
+		int padding = 5;
+		int maxSize = getMaxLabelWidth() + padding;
+		Iterator it = nodePropertiesPanels.values().iterator();
+		if (it.hasNext()) {
+			NodePropertiesPanel panel = (NodePropertiesPanel) it.next();
+			return (new Dimension(maxSize, panel.getPropNameLabelHeight()));
+		}
+		return new Dimension(50, 20);
+	}
+
+	/**
+	 * clear description value panel from any leftover properties (left from previous example)
+	 */
+	public void clear() {
+		Enumeration e = this.nodePropertiesPanels.keys();
+		while (e.hasMoreElements()) {
+			String propertyName = (String) e.nextElement();
+			NodePropertiesPanel propPanel =
+				(NodePropertiesPanel) nodePropertiesPanels.get(propertyName);
+			propPanel.clear();
+		}
+		clonesPanel.clear();
+	}
+
+	/**
+	 * @todo  check if its safe to ignore NoSuchPropertyException (probably is safe
+	 *        for browsing the ontology only. Could be a different story when we
+	 *        are editing it )
+	 */
+	public void setFocus(GraphNode node) {
+		Enumeration e = this.nodePropertiesPanels.keys();
+		while (e.hasMoreElements()) {
+			String propertyName = (String) e.nextElement();
+			try {
+				NodePropertiesPanel propPanel =
+					(NodePropertiesPanel) nodePropertiesPanels.get(
+						propertyName);
+				propPanel.update(node.getProperty(propertyName));
+			} catch (NoSuchPropertyException exc) {
+				// this exception should have been caught when building the graph
+				// we are displaying, so it should be safe to ignore it here
+				System.err.println("NoSuchPropertyException exception: " + exc);
+			}
+		}
+		// deal with clones
+		clonesPanel.update(node.getClones());
+		//this.repaint();
+
+		List fullUrlPropList = new LinkedList();
+		fullUrlPropList.add(node.getFullName());
+		_fullUrlPanel.update(fullUrlPropList);
+
+        Iterator it = Edge.getInboundEdgeNodes(node,1);
+        while (it.hasNext()) {
+          GraphNode parent = (GraphNode) it.next();
+          System.out.println("  parent: " + parent.getName());
         }
-        this.clonesPanel = new ClonesPanel(viewListener);
-   }
+	}
 
-  /**
-   * find max label width
-   */
-   private int getMaxLabelWidth() {
-    Iterator it = nodePropertiesPanels.values().iterator();
-    //Enumeration e = labels.keys();
-    int length = 0;
-    while (it.hasNext()) {
-      NodePropertiesPanel curPanel = (NodePropertiesPanel) it.next();
-      JLabel curLabel = curPanel.getPropNameLabel();
-      //JLabel curLabel = (JLabel) e.nextElement();
-      int width = curPanel.getPropNameLabelWidth();
-      if (width > 0) {
-        length = width;
-      }
-    }
-    return length;
-   }
+	//////////////////////////ViewEventObserver interface implementation////////////////
 
-   /**
-    *
-    */
-   private void setLabelSizes () {
-    Iterator it = nodePropertiesPanels.values().iterator();
-    while (it.hasNext()) {
-      NodePropertiesPanel curPanel = (NodePropertiesPanel) it.next();
-      JLabel curLabel = curPanel.getPropNameLabel();
-      //Dimension d = curLabel.getSize();
-      //System.out.println("d = " + d + ", width = " + maxSize + ", height = " + curPanel.getPropNameLabelHeight());
-      curLabel.setSize(this.propertyNameLabelsDimension);
-      curLabel.setMinimumSize (this.propertyNameLabelsDimension);
-      curLabel.setMaximumSize(this.propertyNameLabelsDimension);
-      curLabel.setPreferredSize(this.propertyNameLabelsDimension);
-    }
-    //this.clonesPanel.setNameLabelSize(this.propertyNameLabelsDimension);
-   }
+	/**
+	 *
+	 */
+	public void focus(GraphNode node) {
+		/*
+		System.out.println();
+		System.out.println(
+			"******* desciptionView got focus for node "
+				+ node.getName()
+				+ ", address = "
+				+ node);
+		*/
+		setFocus(node);
+		
+	}
 
-   /**
-    *
-    */
-   private Dimension calcLabelSize () {
-    int padding = 5;
-    int maxSize = getMaxLabelWidth() + padding;
-    Iterator it = nodePropertiesPanels.values().iterator();
-    if (it.hasNext()) {
-      NodePropertiesPanel panel = (NodePropertiesPanel) it.next();
-      return ( new Dimension(maxSize, panel.getPropNameLabelHeight()) );
-    }
-    return new Dimension(50,20);
-   }
+	/**
+	 *
+	 */
+	public void toggleFold(GraphNode node) {
+	}
 
-  /**
-   * clear description value panel from any leftover properties (left from previous example)
-   */
-  public void clear () {
-      Enumeration e = this.nodePropertiesPanels.keys();
-      while (e.hasMoreElements()) {
-        String propertyName = (String) e.nextElement();
-        NodePropertiesPanel propPanel = (NodePropertiesPanel) nodePropertiesPanels.get(propertyName);
-        propPanel.clear();
-      }
-      clonesPanel.clear();
-  }
-
-  /**
-   * @todo  check if its safe to ignore NoSuchPropertyException (probably is safe
-   *        for browsing the ontology only. Could be a different story when we
-   *        are editing it )
-   */
-  public void setFocus (GraphNode node) {
-      Enumeration e = this.nodePropertiesPanels.keys();
-      while (e.hasMoreElements()) {
-        String propertyName = (String) e.nextElement();
-        try {
-          NodePropertiesPanel propPanel = (NodePropertiesPanel) nodePropertiesPanels.get(propertyName);
-          propPanel.update(node.getProperty(propertyName));      }
-        catch (NoSuchPropertyException exc) {
-          // this exception should have been caught when building the graph
-          // we are displaying, so it should be safe to ignore it here
-          System.err.println("NoSuchPropertyException exception: " + exc);
-        }
-      }
-      // deal with clones
-      clonesPanel.update(node.getClones());
-      //this.repaint();
-  }
-
-
-  //////////////////////////ViewEventObserver interface implementation////////////////
-
-  /**
-   *
-   */
-  public void focus ( GraphNode node) {
-      System.out.println();
-      System.out.println("******* desciptionView got focus for node " + node.getName() + ", address = " + node);
-      setFocus(node);
-      System.out.println();
-  }
-
-  /**
-   *
-   */
-  public void toggleFold ( GraphNode node) {
-  }
-
-  /**
-   *
-   */
-  public void query ( GraphNode node) {
-  }
+	/**
+	 *
+	 */
+	public void query(GraphNode node) {
+	}
 
 }

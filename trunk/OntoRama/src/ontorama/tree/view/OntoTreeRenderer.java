d1109736905f58bdbdf00a22e71d5b0144c04738
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-06-27 04:57:44 +0000

    Adding image generation to render tree nodes with a node image + relation link image, rather then just relation link image

--- trunk/OntoRama/src/ontorama/tree/view/OntoTreeRenderer.java
+++ trunk/OntoRama/src/ontorama/tree/view/OntoTreeRenderer.java
@@ -2,8 +2,12 @@ package ontorama.tree.view;
 
 import java.util.Iterator;
 
+import java.awt.image.*;
+import java.awt.*;
+
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
+import junit.runner.ReloadingTestSuiteLoader;
 import javax.swing.ToolTipManager;
 import javax.swing.JScrollPane;
 import javax.swing.Icon;
@@ -12,6 +16,8 @@ import javax.swing.ImageIcon;
 import java.awt.Image;
 import java.awt.Color;
 import java.awt.Component;
+
+import ontorama.ontologyConfig.ImageMaker;
 
 import ontorama.tree.model.OntoTreeNode;
 import ontorama.tree.model.OntoTreeModel;
@@ -82,8 +88,16 @@ public class OntoTreeRenderer extends DefaultTreeCellRenderer {
 	 *
 	 */
 	protected Icon getIcon (RelationLinkDetails relLinkDetails) {
-		Image image = relLinkDetails.getDisplayImage();
-		Icon icon = new ImageIcon(image);
+		Image relImage = relLinkDetails.getDisplayImage();
+		ImageIcon relImageIcon = new ImageIcon(relImage);
+		ImageObserver relImageObserver = relImageIcon.getImageObserver();
+		
+		
+		Image nodeImage = makeNodeImage(Color.blue, relImage, relImageObserver);
+		
+		
+		//Icon icon = new ImageIcon(relImage);
+		Icon icon = new ImageIcon(nodeImage);
 
 		return icon;
 	}
@@ -100,4 +114,44 @@ public class OntoTreeRenderer extends DefaultTreeCellRenderer {
 		result = result + "Relation Type: " + relLinkName;
 		return result;
 	}
+	
+	/**
+	 * 
+	 */
+    public Image makeNodeImage (Color color, Image relImage,
+    							ImageObserver relImageObserver) {
+    	int relImageWidth = ImageMaker.getWidth();
+    	int relImageHeight = ImageMaker.getHeight();   	
+    	
+        Image image = new BufferedImage(relImageWidth*2,relImageHeight,BufferedImage.TYPE_INT_RGB);
+
+        Graphics g = image.getGraphics();
+		
+        g.setColor(Color.white);
+        g.fillRect(0, 0, relImageWidth*2, relImageHeight*2);
+        g.drawRect(0, 0, relImageWidth*2, relImageHeight*2);
+
+		g.drawImage(relImage, 0, 0, relImageWidth, relImageHeight, relImageObserver);
+
+
+    	int ovalSize = relImageWidth/2;
+    	int ovalX = relImageWidth + relImageWidth/2;
+    	int ovalY = 0;
+        g.setColor(Color.blue);
+        g.fillOval(ovalX, ovalY, ovalSize, ovalSize);
+        g.setColor(Color.black);
+        g.drawOval(ovalX, ovalY, ovalSize+1, ovalSize+1);
+
+
+		int x1 = relImageWidth;
+		int y1 = relImageHeight/2;
+		int x2 = relImageWidth + ovalSize;
+		int y2 = y1;
+		
+		g.setColor(Color.black);
+		g.drawLine(x1,y1,x2,y2);        	
+		
+        return image;
+    }
+	
 }

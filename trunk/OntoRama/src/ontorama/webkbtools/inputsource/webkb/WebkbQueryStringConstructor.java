f68f9fa95666533f6c4b6d44bc9b6552590eae31
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-07-02 01:07:18 +0000

    fixed lines causing compiler warnings (encode() method deprecated)

--- trunk/OntoRama/src/ontorama/webkbtools/inputsource/webkb/WebkbQueryStringConstructor.java
+++ trunk/OntoRama/src/ontorama/webkbtools/inputsource/webkb/WebkbQueryStringConstructor.java
@@ -2,6 +2,7 @@ package ontorama.webkbtools.inputsource.webkb;
 
 import java.util.Iterator;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import ontorama.webkbtools.query.Query;
@@ -20,24 +21,37 @@ public class WebkbQueryStringConstructor  {
    * Get query string for given query
    * @param query
    * @return  WebKB-2 query string ready to append to the cgi script uri.
+   * 
+   * @todo 	we are ignoring UnsupportedEncodingException  because we assume
+   * that at the time of writing this method we would have debugged this,
+   * thus insuring that this encoding is correct. check if this assumption
+   * is acceptable
    */
   public String getQueryString (Query query, String queryOutputFormat) {
     String termName = query.getQueryTypeName();
     Iterator iterator = null;
+    
+    String encoding = "UTF-8";
 
     //queryParamTerm = termName;
     //queryParamRecursLink = ">";
 
     String queryString = "?";
-    queryString = queryString + "term=" + URLEncoder.encode(termName);
-    //queryString = queryString + "term=" + URLEncoder.encode("wn#cat");
-    queryString = queryString + "&recursLink=" + URLEncoder.encode(">");;
-    queryString = queryString + "&format=" + URLEncoder.encode(queryOutputFormat);
-    int queryDepth = query.getDepth();
-    if (queryDepth > 0) {
-    	queryString = queryString + "&depth=" + URLEncoder.encode(String.valueOf(queryDepth));
-    }
-    queryString = queryString + "&noHTML";
+    try {
+		queryString = queryString + "term=" + URLEncoder.encode(termName, encoding);
+	    //queryString = queryString + "term=" + URLEncoder.encode("wn#cat");
+	    queryString = queryString + "&recursLink=" + URLEncoder.encode(">", encoding);;
+	    queryString = queryString + "&format=" + URLEncoder.encode(queryOutputFormat, encoding);
+	    int queryDepth = query.getDepth();
+	    if (queryDepth > 0) {
+	    	queryString = queryString + "&depth=" + URLEncoder.encode(String.valueOf(queryDepth), encoding);
+	    }
+	    queryString = queryString + "&noHTML";
+	} 
+	catch (UnsupportedEncodingException e) {
+		e.printStackTrace();
+	}
+
     System.out.println("\nqueryString = " + queryString + "\n");
     return queryString;
   }

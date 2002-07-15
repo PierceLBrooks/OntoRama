ea387c65ceea0bf9affecd0f3a27f05f3a07b49d
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-07-15 05:56:22 +0000

    Working on displaying of unconnected nodes

---
+++ OntoRama/src/ontorama/view/NodesListViewer.java
@@ -0,0 +1,71 @@
+package ontorama.view;
+
+import java.util.List;
+import java.util.Collection;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.JScrollPane;
+
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+
+import ontorama.model.GraphNode;
+
+/**
+ * @author nataliya
+ *
+ * Display selectable list of nodes
+ */
+public class NodesListViewer extends JFrame {
+	
+	/**
+	 * array of nodes to display
+	 */
+	private GraphNode[] _nodes;
+	
+	/**
+	 * 
+	 */
+	public JList _nodesList;
+	
+	/**
+	 * 
+	 */
+	public NodesListViewer () {
+	}
+	
+	/**
+	 * 
+	 */
+	public void setNodesList (List nodes) {
+		Vector nodesVector = new Vector((Collection) nodes);
+		_nodesList = new JList(nodesVector);
+		_nodesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		JScrollPane scrollPane = new JScrollPane(_nodesList);
+		
+		_nodesList.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged (ListSelectionEvent e) {
+				GraphNode selectedNode = (GraphNode) _nodesList.getSelectedValue();
+				System.out.println("selected node = " + selectedNode.getName());
+			}
+		});
+		
+		getContentPane().add(scrollPane);
+		
+		pack();
+	}
+	
+
+	/**
+	 * 
+	 */
+	public void showList (boolean setVisibleFlag) {
+		setVisible(setVisibleFlag);
+	}
+
+}
+

785777436ee3ed462c424d82cc793a7caea61c2b
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-08-12 00:41:25 +0000

    Attempt to move Hyper Event Handlers into separate classes, so they can leave in the controller package.

---
+++ OntoRama/src/ontorama/hyper/controller/NodeSelectedEventTransformer.java
@@ -0,0 +1,29 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: Aug 12, 2002
+ * Time: 10:27:39 AM
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.hyper.controller;
+
+import org.tockit.events.*;
+import org.tockit.canvas.events.CanvasItemSelectedEvent;
+import ontorama.hyper.view.simple.HyperNodeView;
+import ontorama.controller.NodeSelectedEvent;
+
+public class NodeSelectedEventTransformer implements EventListener {
+    private EventBroker eventBroker;
+
+    public NodeSelectedEventTransformer(EventBroker eventBroker) {
+        this.eventBroker = eventBroker;
+        eventBroker.subscribe(this, CanvasItemSelectedEvent.class, HyperNodeView.class);
+    }
+
+    public void processEvent(Event e) {
+        HyperNodeView nodeView = (HyperNodeView) e.getSubject();
+        eventBroker.processEvent(new NodeSelectedEvent(nodeView.getGraphNode()));
+        System.out.println("processEvent: NodeSelected: " + nodeView);
+    }
+}

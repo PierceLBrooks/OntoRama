b264e12102978bfa1c0e0f44400cbd6cacab1bb4
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-08-14 00:33:38 +0000

    Moving org package into different source dir

---
+++ OntoRama/source/org/tockit/events/tests/TestEventInterface.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright DSTC Pty.Ltd. (http://www.dstc.com), Technische Universitaet Darmstadt
+ * (http://www.tu-darmstadt.de) and the University of Queensland (http://www.uq.edu.au).
+ * Please read licence.txt in the toplevel source directory for licensing information.
+ *
+ * $Id: TestEventInterface.java,v 1.1 2002-08-14 00:33:36 nataliya Exp $
+ */
+package org.tockit.events.tests;
+
+import org.tockit.events.Event;
+
+/**
+ * Test for derived interfaces.
+ */
+public interface TestEventInterface extends Event {
+}

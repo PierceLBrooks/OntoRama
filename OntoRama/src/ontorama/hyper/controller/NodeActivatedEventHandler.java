7da070418b4f8d5d65668fd9629d9e27b6214fb6
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-08-20 06:23:13 +0000

    moved into a separate class

---
+++ OntoRama/src/ontorama/hyper/controller/NodeActivatedEventHandler.java
@@ -0,0 +1,33 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: 20/08/2002
+ * Time: 15:41:09
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.hyper.controller;
+
+import org.tockit.events.*;
+import org.tockit.canvas.events.CanvasItemActivatedEvent;
+import ontorama.hyper.view.simple.SimpleHyperView;
+import ontorama.hyper.view.simple.HyperNodeView;
+
+/**
+ *
+ */
+public class NodeActivatedEventHandler implements EventListener {
+    private SimpleHyperView simpleHyperView;
+
+    public NodeActivatedEventHandler(SimpleHyperView simpleHyperView, EventBroker eventBroker) {
+        this.simpleHyperView = simpleHyperView;
+        eventBroker.subscribe(this, CanvasItemActivatedEvent.class, HyperNodeView.class);
+    }
+
+    public void processEvent(Event e) {
+        HyperNodeView nodeView = (HyperNodeView) e.getSubject();
+        //System.out.println("processEvent: NodeActivated: " + nodeView);
+        simpleHyperView.toggleFold(nodeView.getGraphNode());
+    }
+}
+Argument NodeActivatedEventHandl

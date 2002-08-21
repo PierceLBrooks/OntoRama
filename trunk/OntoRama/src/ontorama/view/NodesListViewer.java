6b78da494653480a70aad83659959f3930215f19
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-08-21 03:07:55 +0000

    changed NodesListViewer to be pull-down list and to live in the toolbar

--- trunk/OntoRama/src/ontorama/view/NodesListViewer.java
+++ trunk/OntoRama/src/ontorama/view/NodesListViewer.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.util.*;
 import java.util.List;
 
@@ -17,85 +20,126 @@ import org.tockit.events.EventBroker;
  *
  * Display selectable list of nodes
  */
-public class NodesListViewer extends JScrollPane {
+public class NodesListViewer extends JComboBox {
 
     /**
-     *
+     * list of currently displayed nodes
      */
-    private JList _nodesList;
+    private List _nodesList = new LinkedList();
 
     /**
-     *
+     * event broker
      */
     private EventBroker _eventBroker;
 
     /**
-     *
+     * default heading/explanation string that should
+     * always be first in the pull-down list.
      */
-    public NodesListViewer(EventBroker eventBroker, List nodes)  {
-        _eventBroker = eventBroker;
-        setNodesList(nodes);
-    }
-
+    private String _defaultHeadingString = "Unconnected Nodes List";
 
     /**
-     *
+     * Viewer to display list of unconnected nodes for current graph
      */
-    public void setNodesList (List nodes) {
+    public NodesListViewer(EventBroker eventBroker)  {
+        super();
+        _eventBroker = eventBroker;
+        addItem(_defaultHeadingString);
+        setSelectedIndex(0);
+        setListViewSize();
 
-        Vector nodesVector = new Vector(sortList(nodes));
-        _nodesList = new JList(nodesVector);
-        _nodesList.setCellRenderer(new NodeListCellRenderer());
-        _nodesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-
-        _nodesList.addListSelectionListener(new ListSelectionListener() {
-            public void valueChanged(ListSelectionEvent e) {
-                GraphNode selectedNode =
-                        (GraphNode) _nodesList.getSelectedValue();
-                //System.out.println("selected node = " + selectedNode);
-                _eventBroker.processEvent(new QueryEvent(selectedNode));
+        addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e) {
+                JComboBox cb = (JComboBox)e.getSource();
+                String selectedNodeName = (String) cb.getSelectedItem();
+                nodeIsSelectedAction(selectedNodeName);
             }
         });
-        setViewportView(_nodesList);
-        repaint();
     }
 
     /**
-     *
+     * set new list of nodes
      */
-    public void showList(boolean setVisibleFlag) {
-        setVisible(setVisibleFlag);
+    public void setNodesList (List nodes) {
+        _nodesList = nodes;
+
+        removeAllItems();
+        addItem(_defaultHeadingString);
+        Iterator it = sortList().iterator();
+        while (it.hasNext()) {
+            String curName = (String) it.next();
+            addItem(curName);
+        }
+    }
+
+
+    private void setListViewSize () {
+        Font font = getFont();
+        FontMetrics fontMetrics = getFontMetrics(font);
+        int strWidth = fontMetrics.stringWidth(_defaultHeadingString);
+        int padding = 5;
+        Dimension d = new Dimension(strWidth + padding*2, fontMetrics.getHeight() + padding*2);
+        setPreferredSize(d);
+        setMaximumSize(d);
+    }
+
+    /**
+     * things we need to do when one of list items is selected.
+     * @param selectedNodeName
+     */
+    private void nodeIsSelectedAction(String selectedNodeName) {
+        if (selectedNodeName == _defaultHeadingString) {
+            return;
+        }
+
+        GraphNode selectedNode = null;
+        Iterator it = _nodesList.iterator();
+        while (it.hasNext()) {
+            GraphNode node = (GraphNode) it.next();
+            if (node.getName().equals(selectedNodeName)) {
+                selectedNode = node;
+            }
+        }
+        _eventBroker.processEvent(new QueryEvent(selectedNode));
     }
 
     /**
      * sort list of nodes in ascending alphabetic order
      * (algorithm is taken from the website http://www.bus.utexas.edu/~plummer/sorting.ppt )
-     * @param nodesList list of nodes to sort
      * @return sorted list of nodes
      */
-    private List sortList(List nodesList)  {
+    private List sortList()  {
         int j;
+        List nodeNamesList = new LinkedList();
         boolean atLeastOneSwap=true;
         while(atLeastOneSwap) {
             atLeastOneSwap = false;
-            for(j=0; j < (nodesList.size()-1); ++j) {
-                GraphNode curNode = (GraphNode) nodesList.get(j);
-                GraphNode nextNode = (GraphNode) nodesList.get(j+1);
+            for(j=0; j < (_nodesList.size()-1); ++j) {
+                GraphNode curNode = (GraphNode) _nodesList.get(j);
+                GraphNode nextNode = (GraphNode) _nodesList.get(j+1);
 
                 String curNodeName = curNode.getName();
                 String nextNodeName = nextNode.getName();
 
                 if(curNodeName.compareToIgnoreCase(nextNodeName) >  0 ) {    // ascending sort
-                    nodesList.set(j, nextNode);
-                    nodesList.set(j+1, curNode);
+                    _nodesList.set(j, nextNode);
+                    _nodesList.set(j+1, curNode);
                     atLeastOneSwap = true;
                 }
             }
         }
-        return nodesList;
+
+        Iterator it = _nodesList.iterator();
+        while (it.hasNext()) {
+            GraphNode node = (GraphNode) it.next();
+            nodeNamesList.add(node.getName());
+        }
+        return nodeNamesList;
     }
 
+    /**
+     * Cell Renderer for nodes list view
+     */
     class NodeListCellRenderer extends DefaultListCellRenderer {
 
         public Component getListCellRendererComponent(

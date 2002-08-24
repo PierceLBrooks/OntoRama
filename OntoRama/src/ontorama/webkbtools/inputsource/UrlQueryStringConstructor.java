c23c43fbf068acf86d13e8bcad6aa543af35bdfe
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-08-24 03:55:23 +0000

    - Introduced general class UrlQueryStringConstructor. WebkbQueryStringConstructor is using it now. Also, CgKbQueryConstructor is using this. - Can now talk to cgi  and retrieve Cg-Kb output. - Hacked DescriptionView to get this working - need to revisit it again

---
+++ OntoRama/src/ontorama/webkbtools/inputsource/UrlQueryStringConstructor.java
@@ -0,0 +1,46 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: 24/08/2002
+ * Time: 11:32:04
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.webkbtools.inputsource;
+
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.net.URLEncoder;
+import java.io.UnsupportedEncodingException;
+
+public class UrlQueryStringConstructor {
+    /**
+     *
+     * @param queryParamTable table containing query parameter names and values
+     *                      keys - parameter names
+     *                      values - parameter values
+     * @return
+     */
+   public String getQueryString(Hashtable queryParamTable) {
+
+        String encoding = "UTF-8";
+        String queryString = "?";
+        try {
+            Enumeration en = queryParamTable.keys();
+            while (en.hasMoreElements()) {
+                String paramName = (String) en.nextElement();
+                String paramValue = (String) queryParamTable.get(paramName);
+                System.out.println("UrlQueryStringConstructor: paramName = " + paramName + ", paramValue = " + paramValue);
+                queryString = queryString + paramName + "=" + URLEncoder.encode(paramValue, encoding);
+                if (en.hasMoreElements()) {
+                    queryString = queryString + "&";
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nqueryString = " + queryString + "\n");
+        return queryString;
+    }
+}
---
+++ OntoRama/src/ontorama/webkbtools/inputsource/cgkb/CgKbSource.java
@@ -0,0 +1,71 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: 24/08/2002
+ * Time: 11:53:20
+ * To change template for new class use
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.webkbtools.inputsource.cgkb;
+
+import ontorama.webkbtools.inputsource.*;
+import ontorama.webkbtools.query.Query;
+import ontorama.webkbtools.util.SourceException;
+import ontorama.webkbtools.util.CancelledQueryException;
+import ontorama.ontologyConfig.RelationLinkDetails;
+import ontorama.OntoramaConfig;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class CgKbSource implements Source {
+
+    public SourceResult getSourceResult(String uri, Query query) throws SourceException, CancelledQueryException {
+        UrlQueryStringConstructor queryStringConstructor = new UrlQueryStringConstructor();
+        List relLinksList = query.getRelationLinksList();
+        if (relLinksList.isEmpty()) {
+            System.err.println("Query relation links list is empty!");
+        }
+        Iterator it = relLinksList.iterator();
+        String allReadersString="";
+        while (it.hasNext()) {
+            Integer relLinkType = (Integer) it.next();
+
+            String readerString = "";
+            Hashtable paramTable = new Hashtable();
+
+            paramTable.put("node",query.getQueryTypeName());
+            RelationLinkDetails relDetails = OntoramaConfig.getRelationLinkDetails(relLinkType.intValue());
+
+            paramTable.put("rel", relDetails.getLinkName());
+            int depth = 1;
+            Integer depthInt = new Integer(depth);
+            paramTable.put("depth",depthInt.toString());
+
+            String queryString = queryStringConstructor.getQueryString(paramTable);
+            String fullUrl = uri + queryString;
+            URL url = null;
+            try {
+                url = new URL(fullUrl);
+                URLConnection connection = url.openConnection();
+                Reader reader = new InputStreamReader(connection.getInputStream());
+                BufferedReader br = new BufferedReader(reader);
+                String line;
+                while ((line=br.readLine()) != null) {
+                    readerString = readerString + line + "\n";
+                }
+            }
+            catch ( MalformedURLException mue) {
+                throw new SourceException("Source Url " + url +
+                " is malformed");
+            }
+            catch ( IOException ioe) {
+                throw new SourceException("Couldn't retrieve data from source " + fullUrl);
+            }
+            allReadersString = allReadersString + readerString;
+        }
+        StringReader strReader = new StringReader(allReadersString);
+        return new SourceResult(true, strReader, query);
+    }
+}

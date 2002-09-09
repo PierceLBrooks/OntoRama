131797ba3dffe13f9086ca29a4a486e3b2ff9126
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-09-09 03:28:32 +0000

    Renamed a few things

--- OntoRama/src/ontorama/model/NodeTypeInterface.java
+++ OntoRama/src/ontorama/model/NodeTypeInterface.java
@@ -10,7 +10,7 @@ package ontorama.model;
 
 import java.awt.*;
 
-public interface NodeType {
+public interface NodeTypeInterface {
     public String getNodeType ();
     public Color getDisplayColor();
 }

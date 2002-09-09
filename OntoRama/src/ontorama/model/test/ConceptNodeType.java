cf031a5f46d1f7f1a02b6ed9e00a52d5c3df33cf
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-09-09 03:20:51 +0000

    first import

---
+++ OntoRama/src/ontorama/model/test/ConceptNodeType.java
@@ -0,0 +1,26 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: Sep 9, 2002
+ * Time: 1:12:34 PM
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.model.test;
+
+import ontorama.model.NodeType;
+
+import java.awt.*;
+
+public class ConceptNodeType implements NodeType {
+    private String _nodeTypeName = "Concept Type";
+    private Color _displayColor = Color.blue;
+
+    public String getNodeType() {
+        return _nodeTypeName;
+    }
+
+    public Color getDisplayColor() {
+        return _displayColor;
+    }
+}

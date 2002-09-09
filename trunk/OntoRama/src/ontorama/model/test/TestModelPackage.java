c70093adcd886f9fad71219989c9b16ae37cc26a
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-09-09 01:07:30 +0000

    Moved tests from package ontorama.model into ontorama.model.test

---
+++ trunk/OntoRama/src/ontorama/model/test/TestModelPackage.java
@@ -0,0 +1,33 @@
+package ontorama.model.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import ontorama.model.test.TestEdge;
+import ontorama.model.test.TestGraph;
+import ontorama.model.test.TestGraphNode;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: DSTC</p>
+ * @author nataliya
+ * @version 1.0
+ */
+
+public class TestModelPackage extends TestCase {
+
+    public TestModelPackage(String name) {
+        super(name);
+    }
+
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("ontorama.model");
+        suite.addTest(new TestSuite(TestEdge.class));
+        suite.addTest(new TestSuite(TestGraphNode.class));
+        suite.addTest(new TestSuite(TestGraph.class));
+        return suite;
+    }
+}

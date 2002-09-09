79517aa7be888d5d48dd8815f8aa1012efb04424
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-09-09 03:20:51 +0000

    first import

---
+++ trunk/OntoRama/src/ontorama/model/NodeType.java
@@ -0,0 +1,16 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: Sep 9, 2002
+ * Time: 1:07:04 PM
+ * To change template for new interface use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.model;
+
+import java.awt.*;
+
+public interface NodeType {
+    public String getNodeType ();
+    public Color getDisplayColor();
+}

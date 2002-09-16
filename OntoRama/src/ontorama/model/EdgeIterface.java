12fb891d98a1a30470758a3f36308755d134e734
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-09-16 01:54:33 +0000

    first import

---
+++ OntoRama/src/ontorama/model/EdgeIterface.java
@@ -0,0 +1,19 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: Sep 16, 2002
+ * Time: 11:50:51 AM
+ * To change template for new interface use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.model;
+
+import ontorama.ontologyConfig.RelationLinkDetails;
+
+public interface EdgeIterface {
+    GraphNode getFromNode();
+
+    GraphNode getToNode();
+
+    RelationLinkDetails getEdgeType();
+}

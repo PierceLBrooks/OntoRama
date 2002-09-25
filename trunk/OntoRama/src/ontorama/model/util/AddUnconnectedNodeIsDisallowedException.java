98b7651c06cc2b4bbeee90db00845ff231289ff4
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-09-25 01:58:45 +0000

    first import

---
+++ trunk/OntoRama/src/ontorama/model/util/AddUnconnectedNodeIsDisallowedException.java
@@ -0,0 +1,17 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: Sep 25, 2002
+ * Time: 11:55:13 AM
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.model.util;
+
+import ontorama.model.Node;
+
+public class AddUnconnectedNodeIsDisallowedException extends Exception  {
+    public AddUnconnectedNodeIsDisallowedException (Node node) {
+        super("Can't add unconnected node " + node.getName());
+    }
+}

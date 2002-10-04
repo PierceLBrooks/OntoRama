cfb666cd32e4c9020ea0f4b423708e0551e80aa8
pbecker <pbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-04 05:44:42 +0000

    New nodes are now focused.
    This is pretty jumpy, but I don't know how to handle this
    behaviour yet since the graph is completely relayouted
    everytime it is changed.

--- OntoRama/src/ontorama/hyper/controller/NodeContextMenuHandler.java
+++ OntoRama/src/ontorama/hyper/controller/NodeContextMenuHandler.java
@@ -8,7 +8,10 @@ import org.tockit.canvas.events.CanvasItemContextMenuRequestEvent;
 import ontorama.hyper.view.simple.SimpleHyperView;
 import ontorama.hyper.view.simple.HyperNodeView;
 import ontorama.OntoramaConfig;
+import ontorama.controller.NodeSelectedEvent;
+import ontorama.ontologyConfig.EdgeTypeDisplayInfo;
 import ontorama.model.EdgeType;
+import ontorama.model.Node;
 
 import javax.swing.*;
 import java.awt.geom.Point2D;
@@ -22,10 +25,12 @@ import java.util.Iterator;
  */
 public class NodeContextMenuHandler implements EventListener {
     private SimpleHyperView simpleHyperView;
+    private EventBroker eventBroker;
 
     public NodeContextMenuHandler(SimpleHyperView simpleHyperView, EventBroker eventBroker) {
         this.simpleHyperView = simpleHyperView;
         eventBroker.subscribe(this, CanvasItemContextMenuRequestEvent.class, HyperNodeView.class);
+        this.eventBroker = eventBroker;
     }
 
     public void processEvent(Event e) {
@@ -36,13 +41,19 @@ public class NodeContextMenuHandler implements EventListener {
         List edgeTypes = OntoramaConfig.getEdgeTypesList();
         for (Iterator iterator = edgeTypes.iterator(); iterator.hasNext();) {
             final EdgeType edgeType = (EdgeType) iterator.next();
-            menuItem = new JMenuItem(edgeType.getName());
-            menuItem.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    new NewRelatedNodeCreator(simpleHyperView, nodeView.getGraphNode(), edgeType);
-                }
-            });
-            menu.add(menuItem);
+            EdgeTypeDisplayInfo displayInfo = OntoramaConfig.getEdgeDisplayInfo(edgeType);
+            if(displayInfo.isDisplayInGraph()) {
+                menuItem = new JMenuItem(edgeType.getName());
+                menuItem.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        NewRelatedNodeCreator creator =
+                                        new NewRelatedNodeCreator(simpleHyperView, nodeView.getGraphNode(), edgeType);
+                        Node newNode = creator.createNewRelatedNode();
+                        eventBroker.processEvent(new NodeSelectedEvent(newNode));
+                    }
+                });
+                menu.add(menuItem);
+            }
         }
         Point2D awtPos = ev.getAWTPosition();
         menu.show(simpleHyperView, (int) awtPos.getX(), (int) awtPos.getY());

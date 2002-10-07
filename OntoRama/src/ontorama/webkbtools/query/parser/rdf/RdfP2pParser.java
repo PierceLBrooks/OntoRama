bc19fc0e67db363d5aa359d71bf6f7471d07099b
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-07 07:34:50 +0000

    improved RDF P2P parser: it can now handle asserted and rejected nodes. Not sure if the same parser will work for more verbose P2P RDF, still have to test.
    TestCases are updated accordinally.

--- OntoRama/src/ontorama/webkbtools/query/parser/rdf/RdfP2pParser.java
+++ OntoRama/src/ontorama/webkbtools/query/parser/rdf/RdfP2pParser.java
@@ -1,25 +1,25 @@
 package ontorama.webkbtools.query.parser.rdf;
 
-import ontorama.webkbtools.query.parser.Parser;
-import ontorama.webkbtools.query.parser.ParserResult;
-import ontorama.webkbtools.util.ParserException;
-import ontorama.webkbtools.util.NoSuchRelationLinkException;
-import ontorama.model.EdgeType;
-import ontorama.OntoramaConfig;
-import ontorama.ontologyConfig.RdfMapping;
-
-import java.util.*;
-import java.io.Reader;
-import java.security.AccessControlException;
-
-import com.hp.hpl.mesa.rdf.jena.model.*;
-import com.hp.hpl.mesa.rdf.jena.model.Property;
-import com.hp.hpl.mesa.rdf.jena.mem.ModelMem;
-import com.hp.hpl.mesa.rdf.jena.common.SelectorImpl;
-import com.hp.hpl.mesa.rdf.jena.common.PropertyImpl;
 import com.hp.hpl.jena.rdf.query.QueryResults;
 import com.hp.hpl.jena.rdf.query.ResultBinding;
-import com.hp.hpl.jena.rdf.query.Value;
+import com.hp.hpl.mesa.rdf.jena.common.PropertyImpl;
+import com.hp.hpl.mesa.rdf.jena.mem.ModelMem;
+import com.hp.hpl.mesa.rdf.jena.model.*;
+
+import ontorama.OntoramaConfig;
+import ontorama.backends.p2p.model.*;
+import ontorama.model.*;
+import ontorama.ontologyConfig.RdfMapping;
+import ontorama.webkbtools.query.parser.Parser;
+import ontorama.webkbtools.query.parser.ParserResult;
+import ontorama.webkbtools.util.NoSuchRelationLinkException;
+import ontorama.webkbtools.util.ParserException;
+
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.AccessControlException;
+import java.util.*;
 
 /*
  * Created by IntelliJ IDEA.
@@ -80,21 +80,27 @@ public class RdfP2pParser implements Parser {
             ResIterator subjectsIt = model.listSubjects();
             while (subjectsIt.hasNext()) {
                 System.out.println("-----------------------------------------------");
-                Resource res = subjectsIt.next();
-                System.out.println("subject: " + res + ", isAnon = " + res.isAnon());
-                StmtIterator it = res.listProperties();
+                Resource resource = subjectsIt.next();
+                System.out.println("subject: " + resource + ", isAnon = " + resource.isAnon());
+                StmtIterator it = resource.listProperties();
                 while (it.hasNext()) {
                     Statement st = it.next();
                     System.out.println("---" + st);
-                    if (res.isAnon()) {
-                        if (_processedAnonymousNodes.contains(res)) {
-                            System.out.println("this anon. node is already processed");
+                    if (st.getPredicate().toString().endsWith("rdf-syntax-ns#type")) {
+                        /// @todo hack: should find a better way to deal with this.
+                        continue;
+                    }
+                    if (resource.isAnon()) {
+                        System.out.println("SUBJECT IS ANONYMOUS");
+                        if (_processedAnonymousNodes.contains(resource)) {
+                            System.out.println("this resource is already processed");
                             continue;
                         }
-                        processAnonymousNode(st, model);
-                        _processedAnonymousNodes.add(res);
+                        processAnonymousSubject(st, model);
+                        _processedAnonymousNodes.add(resource);
                     }
                     else {
+                        System.out.println("RESOURCE IS NOT ANONYMOUS");
                         processStatement(st);
                     }
                 }
@@ -112,30 +118,58 @@ public class RdfP2pParser implements Parser {
             relExc.printStackTrace();
             throw new ParserException("Unrecognized EdgeType: " + relExc.getMessage());
         }
+        catch (URISyntaxException uriExc) {
+            uriExc.printStackTrace();
+            throw new ParserException("Found invalid Asserter or Rejector URI in source :" + uriExc.getMessage());
+        }
         ParserResult result = new ParserResult(new LinkedList(_nodesHash.values()), _edgesList);
         return result;
     }
 
-    private void processStatement (Statement st) {
+    protected void processStatement (Statement st) throws NoSuchRelationLinkException, URISyntaxException {
         Resource subject = st.getSubject();
         Property predicate = st.getPredicate();
         RDFNode object = st.getObject();
 
+        /// assumption is that we already processed this statement if it contains
+        // anonymous object - it would have been processed in processAnonymousSubject()
+        // method because we query for this anonymous node there.
+        /// @todo looks like a hack ;) perhaps better to 'cross off' statements off a list once they are processed.
+        if (_processedAnonymousNodes.contains(object)) {
+            System.out.println("this object is already processed as anonymous");
+            return;
+        }
+
+        String predicateStr = predicate.toString();
+
+        if (predicateStr.endsWith(_rdf_tag_asserted)) {
+            P2PNode subjectNode = getNodeForName(subject.toString());
+            subjectNode.addAssertion(new URI(object.toString()));
+            return;
+        }
+        if (predicateStr.endsWith(_rdf_tag_rejected)) {
+            P2PNode subjectNode = getNodeForName(subject.toString());
+            subjectNode.addRejection(new URI(object.toString()));
+            return;
+        }
+
+        P2PEdge edge = mapEdgeIntoModel(subject.toString(), predicate.toString(), object.toString());
+        addEdgeToEdgesList(edge);
     }
 
-    private void processAnonymousNode(Statement st, Model model) throws NoSuchRelationLinkException {
+    private void processAnonymousSubject(Statement st, Model model) throws NoSuchRelationLinkException,
+                                            URISyntaxException {
         RDFNode object = st.getObject();
 
         // var 'x' is our anonymous node
 
-        String mainNodeName = findNameForAnonymousNode(object, model);
+        String mainNodeName = findNameForAnonymousSubject(object, model);
         com.hp.hpl.jena.rdf.query.Query query;
         QueryResults result;
         List resBindList;
         Iterator resBindIterator;
 
-        System.out.println("RESULT: ");
-        System.out.println("Node = " + mainNodeName);
+        System.out.println("anonymous node is mapped to " + mainNodeName);
 
         String subject1VariableName = "subject1";
         String predicate1VariableName = "predicate1";
@@ -154,39 +188,19 @@ public class RdfP2pParser implements Parser {
         result = query.exec(queryStr3, model);
         resBindList = result.getAll();
         resBindIterator = resBindList.iterator();
+        System.out.println("***");
         while (resBindIterator.hasNext()) {
             ResultBinding cur = (ResultBinding) resBindIterator.next();
-            System.out.println(cur.getValue(subject1VariableName) + " -> " + cur.getValue(predicate1VariableName) + " -> " + cur.getValue("x"));
-            System.out.println(cur.getValue("x") + " -> " + cur.getValue(predicate2VariableName) + " -> " + cur.getValue(object2VariableName));
-            mapTripleIntoGraphModel(cur.getValue(subject1VariableName).toString(),cur.getValue(predicate1VariableName).toString(),cur.getValue("x").toString());
-            mapTripleIntoGraphModel(cur.getValue("x").toString(), cur.getValue(predicate2VariableName).toString(), cur.getValue(object2VariableName).toString());
+            String subject1 = cur.getValue(subject1VariableName).toString();
+            String predicate1 = cur.getValue(predicate1VariableName).toString();
+            String predicate2 = cur.getValue(predicate2VariableName).toString();
+            String object2 = cur.getValue(object2VariableName).toString();
+            mapReifiedStatementIntoModel(subject1, predicate1, mainNodeName, predicate2, object2);
         }
+        System.out.println("***");
     }
 
-    private void mapTripleIntoGraphModel(String subjectStr, String predicateStr, String objectStr) throws NoSuchRelationLinkException {
-        if (predicateStr.toString().endsWith("value")) {
-            // we already processed this one above in findNameForAnonymousNode() method
-            return;
-        }
-        if (predicateStr.endsWith(_rdf_tag_asserted)) {
-            //
-            return;
-        }
-        if (predicateStr.endsWith(_rdf_tag_rejected)) {
-            //
-            return;
-        }
-        EdgeTypeExtended edgeTypeExt = getEdgeTypeForRdfTag(predicateStr);
-        EdgeType edgeType = edgeTypeExt.getEdgeType();
-        if (!edgeTypeExt.getIsReverse()) {
-
-        }
-        else {
-
-        }
-    }
-
-    private String findNameForAnonymousNode(RDFNode object, Model model) {
+    private String findNameForAnonymousSubject(RDFNode object, Model model) {
         String mainNodeName = null;
         String queryStr1 = "SELECT ?x WHERE \n";
         queryStr1 = queryStr1 + " (?x, <" + _rdfValueProp + ">, ?z), \n";
@@ -205,6 +219,77 @@ public class RdfP2pParser implements Parser {
             mainNodeName = cur.getValue("z").toString();
         }
         return mainNodeName;
+    }
+
+
+    /**
+     * first statement is reified statement, second statement is a statement ABOUT the first statement.
+     * @param subject1
+     * @param predicate1
+     * @param object1
+     * @param predicate2
+     * @param object2
+     * @throws NoSuchRelationLinkException
+     */
+    private void mapReifiedStatementIntoModel(String subject1, String predicate1, String object1,
+                                            String predicate2, String object2)
+                                            throws NoSuchRelationLinkException, URISyntaxException {
+        System.out.println(subject1 + ", -> " + predicate1 + " -> " + object1);
+        System.out.println(object1 + ", -> " + predicate2 + " -> " + object2);
+
+        P2PEdge edge = mapEdgeIntoModel(subject1, predicate1, object1);
+
+        if (predicate2.toString().endsWith("value")) {
+            // we already processed this one above in findNameForAnonymousSubject() method
+            return;
+        }
+        if (predicate2.endsWith(_rdf_tag_asserted)) {
+            edge.addAssertion(new URI(object2));
+        }
+        if (predicate2.endsWith(_rdf_tag_rejected)) {
+            edge.addRejection(new URI(object2));
+        }
+        addEdgeToEdgesList(edge);
+    }
+
+    private P2PEdge mapEdgeIntoModel(String subjectStr, String predicateStr, String objectStr) throws NoSuchRelationLinkException {
+        EdgeTypeExtended edgeTypeExt = getEdgeTypeForRdfTag(predicateStr);
+        EdgeType edgeType = edgeTypeExt.getEdgeType();
+        P2PNode node1 = getNodeForName(subjectStr);
+        P2PNode node2 = getNodeForName(objectStr);
+        P2PEdge edge = null;
+        if (!edgeTypeExt.getIsReverse()) {
+            edge = findEdgeInEdgesList(subjectStr, objectStr, edgeType);
+            if (edge == null) {
+                edge = new P2PEdgeImpl(node1, node2, edgeType);
+            }
+        }
+        else {
+            edge = findEdgeInEdgesList(objectStr, subjectStr, edgeType);
+            if (edge == null ) {
+                edge = new P2PEdgeImpl(node2, node1, edgeType);
+            }
+        }
+        return edge;
+    }
+
+    private void addEdgeToEdgesList (P2PEdge edge ) {
+        P2PEdge foundEdge = findEdgeInEdgesList(edge.getFromNode().getName(), edge.getToNode().getName(), edge.getEdgeType());
+        if (foundEdge == null ) {
+            _edgesList.add(edge);
+        }
+    }
+
+
+    private P2PNode getNodeForName (String nodeName) {
+        if (_nodesHash.containsKey(nodeName)) {
+            return (P2PNode) _nodesHash.get(nodeName);
+        }
+        else {
+            P2PNode node = new P2PNodeImpl(nodeName, nodeName);
+            _nodesHash.put(nodeName, node);
+            return node;
+        }
     }
 
     private EdgeTypeExtended getEdgeTypeForRdfTag (String predicateStr) throws NoSuchRelationLinkException {
@@ -229,6 +314,23 @@ public class RdfP2pParser implements Parser {
                             System.out.println("Dont' know about property '" + predicateStr + "'");
                             System.exit(-1);
                         }
+                }
+            }
+        }
+        return null;
+    }
+
+    private P2PEdge findEdgeInEdgesList(String fromNodeName, String toNodeName, EdgeType edgeType) {
+        Iterator it = _edgesList.iterator();
+        while (it.hasNext()) {
+            P2PEdge cur = (P2PEdge) it.next();
+            if (cur.getEdgeType().equals(edgeType)) {
+                Node fromNode = cur.getFromNode();
+                Node toNode = cur.getToNode();
+                if (fromNode.getName().equals(fromNodeName)) {
+                    if (toNode.getName().equals(toNodeName)) {
+                        return cur;
+                    }
                 }
             }
         }

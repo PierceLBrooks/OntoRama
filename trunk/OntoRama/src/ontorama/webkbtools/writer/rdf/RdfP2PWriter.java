9c49050c5d570434cfd6f7967d1593e89be01490
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-08 01:35:51 +0000

    first import

---
+++ trunk/OntoRama/src/ontorama/webkbtools/writer/rdf/RdfP2PWriter.java
@@ -0,0 +1,10 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: 8/10/2002
+ * Time: 11:33:27
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.webkbtools.writer.rdf;
+public class RdfP2PWriter { }
---
+++ trunk/OntoRama/src/ontorama/webkbtools/writer/rdf/test/TestRdfP2PWriter.java
@@ -0,0 +1,10 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: 8/10/2002
+ * Time: 11:34:38
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.webkbtools.writer.rdf.test;
+public class TestRdfP2PWriter { }

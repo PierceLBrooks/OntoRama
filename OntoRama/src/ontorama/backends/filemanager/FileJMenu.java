45959da001b3c6d16494b19c3ab4564d81ab375f
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-10 00:49:15 +0000

    Added Menu Backends containing File Backend submenu with corresponding menu items. Added calls to file chooser to open and save files.

--- OntoRama/src/ontorama/backends/filemanager/FileJMenu.java
+++ OntoRama/src/ontorama/backends/filemanager/FileJMenu.java
@@ -1,7 +1,9 @@
 package ontorama.backends.filemanager;
 
+import ontorama.OntoramaConfig;
+
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /*
  * Created by IntelliJ IDEA.
@@ -13,17 +15,48 @@ import java.awt.*;
 public class FileJMenu extends JMenu {
     private FileBackend _fileBackend = null;
 
-    private String _menuName = "File Bck";
+    private String _menuName = "File Backend";
     private String _menuItemOpen = "Open...";
     private String _menuItemSave = "Save...";
+
+    private JFileChooser _fileChooser;
 
     public FileJMenu(FileBackend fb){
         super();
 
         _fileBackend = fb;
+        _fileChooser = new JFileChooser();
 
         setText(_menuName);
 
+        Action openAction = new AbstractAction(_menuItemOpen) {
+            public void actionPerformed(ActionEvent e) {
+                openFile();
+            }
+        };
+
+        Action saveAction = new AbstractAction(_menuItemSave) {
+            public void actionPerformed(ActionEvent e) {
+                saveFile();
+            }
+        };
+
+        add(openAction);
+        add(saveAction);
+    }
+
+    private void openFile () {
+        int returnValue = _fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            _fileBackend.loadFile(_fileChooser.getSelectedFile().getAbsolutePath());
+        }
+    }
+
+    private void saveFile () {
+        int returnValue = _fileChooser.showSaveDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            _fileBackend.saveFile(_fileChooser.getSelectedFile().getAbsolutePath());
+        }
     }
 
 }

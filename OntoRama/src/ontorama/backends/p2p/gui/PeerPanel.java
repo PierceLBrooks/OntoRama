63f2ce1a74399cbcb72a9ac172c7856cdc7cbe7b
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-11 06:30:55 +0000

    added UI components to PeerPanel, however everything doesn't work as expected yet ;)

--- OntoRama/src/ontorama/backends/p2p/gui/PeerPanel.java
+++ OntoRama/src/ontorama/backends/p2p/gui/PeerPanel.java
@@ -1,6 +1,15 @@
 package ontorama.backends.p2p.gui;
 
 import javax.swing.*;
+import java.util.Vector;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.LinkedList;
+import java.awt.CardLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 /*
  * Created by IntelliJ IDEA.
@@ -10,8 +19,84 @@ import javax.swing.*;
  * To change this template use Options | File Templates.
  */
 public class PeerPanel extends JPanel {
+
+    private Hashtable _groupToPanelMapping;
+
+    private Vector _groupsVector;
+
+    private JComboBox _comboBox;
+    private JPanel _cardPanel;
+    private CardLayout _cardLayout;
+
     public PeerPanel() {
         super();
-        add(new JLabel("Peer panel here"));
+        _groupToPanelMapping = new Hashtable();
+        _groupsVector = new Vector();
+
+        _comboBox = new JComboBox(_groupsVector);
+        _comboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String selectedGroupName = (String) _comboBox.getSelectedItem();
+                if (selectedGroupName == null) {
+                    return;
+                }
+                if (_groupToPanelMapping.size() == 0 ) {
+                    return;
+                }
+                GroupPanel groupPanel = (GroupPanel) _groupToPanelMapping.get(selectedGroupName);
+                _cardLayout.show(groupPanel, groupPanel.getName());
+
+            }
+        });
+
+        _cardLayout = new CardLayout(10, 10);
+        _cardPanel = new JPanel(_cardLayout);
+
+        add(new JLabel("Groups "), BorderLayout.NORTH);
+        add(_comboBox, BorderLayout.CENTER);
+        add(_cardPanel, BorderLayout.SOUTH);
+
+        addGroup("testId", "testName", "testGroup");
+    }
+
+    public void addGroup(String peerId, String peerName, String groupId) {
+        _comboBox.add(new JLabel(groupId));
+
+        GroupPanel groupPanel = new GroupPanel(groupId);
+        _cardLayout.addLayoutComponent(groupPanel, groupId);
+        groupPanel.addPeer(peerId);
+
+        _groupToPanelMapping.put(peerName, groupPanel);
+        repaint();
+    }
+
+    private class GroupPanel extends JPanel {
+        Vector peersList;
+        JList jlist;
+
+        public GroupPanel(String groupName) {
+            peersList = new Vector();
+            jlist = new JList(peersList);
+            add(jlist);
+        }
+
+        public void addPeer (String name) {
+            System.out.println("addPeer for name " + name);
+            peersList.add(name);
+            updateJList();
+            repaint();
+        }
+
+        public void removePeer (String name) {
+            peersList.remove(name);
+            updateJList();
+            repaint();
+        }
+
+        private void updateJList() {
+            remove(jlist);
+            jlist = new JList(peersList);
+            add(jlist);
+        }
     }
 }

1e3cc91d9f9c2d150bf83bf827afa6ceeac0ebbb
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-13 08:50:04 +0000

    fixed layout

--- OntoRama/src/ontorama/backends/p2p/gui/PeerPanel.java
+++ OntoRama/src/ontorama/backends/p2p/gui/PeerPanel.java
@@ -5,8 +5,7 @@ import java.util.Vector;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.LinkedList;
-import java.awt.CardLayout;
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -20,6 +19,9 @@ import java.awt.event.ActionEvent;
  */
 public class PeerPanel extends JPanel {
 
+    /**
+     * holds mapping from groupId to the corresponding groupPanel
+     */
     private Hashtable _groupToPanelMapping;
 
     private Vector _groupsVector;
@@ -44,30 +46,47 @@ public class PeerPanel extends JPanel {
                     return;
                 }
                 GroupPanel groupPanel = (GroupPanel) _groupToPanelMapping.get(selectedGroupName);
-                _cardLayout.show(groupPanel, groupPanel.getName());
-
+                _cardLayout.show(_cardPanel, groupPanel.getName());
+                groupPanel.setVisible(true);
             }
         });
 
+        _cardPanel = new JPanel();
         _cardLayout = new CardLayout(10, 10);
-        _cardPanel = new JPanel(_cardLayout);
+        _cardPanel.setLayout(_cardLayout);
+
+        _cardPanel.setBorder(BorderFactory.createEtchedBorder());
 
         add(new JLabel("Groups "), BorderLayout.NORTH);
         add(_comboBox, BorderLayout.CENTER);
         add(_cardPanel, BorderLayout.SOUTH);
 
-        addGroup("testId", "testName", "testGroup");
+        addGroup("testId", "testGroup");
+        addGroup("id2", "group2");
+        addGroup("id3", "group3");
+
+        addPeer("peerId1", "peerName1", "id2");
+        addPeer("peerId2", "peerName2", "id2");
+        addPeer("peerId3", "peerName3", "id2");
+        addPeer("peerId4", "peerName4", "id2");
+        addPeer("peerId5", "peerName5", "id2");
     }
 
-    public void addGroup(String peerId, String peerName, String groupId) {
-        _comboBox.add(new JLabel(groupId));
+    public void addGroup(String groupId, String groupName) {
+        _groupsVector.add(groupId);
 
         GroupPanel groupPanel = new GroupPanel(groupId);
-        _cardLayout.addLayoutComponent(groupPanel, groupId);
-        groupPanel.addPeer(peerId);
 
-        _groupToPanelMapping.put(peerName, groupPanel);
+        _cardLayout.addLayoutComponent(groupPanel, groupId);
+        _cardPanel.add(groupId, groupPanel);
+
+        _groupToPanelMapping.put(groupId, groupPanel);
         repaint();
+    }
+
+    public void addPeer (String peerId, String peerName, String groupId) {
+        GroupPanel groupPanel = (GroupPanel) _groupToPanelMapping.get(groupId);
+        groupPanel.addPeer(peerName);
     }
 
     private class GroupPanel extends JPanel {
@@ -76,27 +95,21 @@ public class PeerPanel extends JPanel {
 
         public GroupPanel(String groupName) {
             peersList = new Vector();
+            setName(groupName);
             jlist = new JList(peersList);
-            add(jlist);
+            JScrollPane scrollPanel = new JScrollPane(jlist);
+            add(scrollPanel);
         }
 
         public void addPeer (String name) {
             System.out.println("addPeer for name " + name);
             peersList.add(name);
-            updateJList();
             repaint();
         }
 
         public void removePeer (String name) {
             peersList.remove(name);
-            updateJList();
             repaint();
-        }
-
-        private void updateJList() {
-            remove(jlist);
-            jlist = new JList(peersList);
-            add(jlist);
         }
     }
 }

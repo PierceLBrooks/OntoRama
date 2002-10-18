2316dfd9aacafe180af9f320a26858b5e58e0ab0
henrika <henrika@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-18 03:33:01 +0000

    Added the reset Change Panel

--- OntoRama/src/ontorama/backends/p2p/gui/action/ActionResetChangePanel.java
+++ OntoRama/src/ontorama/backends/p2p/gui/action/ActionResetChangePanel.java
@@ -1,6 +1,11 @@
 package ontorama.backends.p2p.gui.action;
 
 import javax.swing.*;
+
+import ontorama.backends.p2p.P2PBackend;
+import ontorama.backends.p2p.gui.ChangePanel;
+import ontorama.backends.p2p.p2pmodule.P2PSender;
+
 import java.awt.event.ActionEvent;
 
 /*
@@ -11,13 +16,18 @@ import java.awt.event.ActionEvent;
  * To change this template use Options | File Templates.
  */
 public class ActionResetChangePanel extends AbstractAction {
+     private P2PBackend _p2pBackend;
 
-    public ActionResetChangePanel(String name) {
+    public ActionResetChangePanel(String name, P2PBackend p2pBackend) {
         super(name);
+        _p2pBackend = p2pBackend;
     }
 
     public void actionPerformed(ActionEvent e) {
-        System.out.println("...action change panel");
+          System.out.println("An empty method is preformed");
+       ChangePanel changePanel = (ChangePanel) _p2pBackend.getPanels().get(1);
+       changePanel.empty();
+       
     }
 
 

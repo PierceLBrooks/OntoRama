5bac18eb0f33594713fa14e853dad8f2e1326e05
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-10-22 08:04:24 +0000

    file manager can almost load an ontology file (root is hardcoded, need to change).

---
+++ trunk/OntoRama/src/ontorama/controller/GeneralQueryEvent.java
@@ -0,0 +1,27 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: 22/10/2002
+ * Time: 16:55:21
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package ontorama.controller;
+
+import org.tockit.events.Event;
+import ontorama.webkbtools.query.Query;
+
+public class GeneralQueryEvent implements Event {
+
+    private Query query;
+
+    public GeneralQueryEvent(Query subject) {
+        this.query = subject;
+    }
+
+
+    public Object getSubject() {
+        return query;
+    }
+
+}

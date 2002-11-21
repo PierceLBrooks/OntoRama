433ae6d20ff8fd4a1125ec48df637bb455816deb
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-11-21 01:53:15 +0000

    First Import

---
+++ trunk/OntoRama/src/ontorama/util/validate/schema/Validator.java
@@ -0,0 +1,44 @@
+package ontorama.util.validator.schema;
+
+import java.io.*;
+
+
+import org.jdom.input.SAXBuilder;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+
+import org.apache.xerces.parsers.SAXParser; 
+
+public class Validator {
+
+	public Validator (String schemaLoc, String instanceLoc) {
+		System.out.println("shema file: " + schemaLoc);
+		System.out.println("instance file: " + instanceLoc);
+
+		try {
+
+			SAXBuilder builder = new SAXBuilder("org.apache.xerces.parsers.SAXParser", true);
+			builder.setFeature("http://apache.org/xml/features/validation/schema", true);
+			builder.setProperty( 
+				"http://apache.org/xml/properties/schema/external-schemaLocation",
+                    		schemaLoc);
+			Document doc = builder.build(instanceLoc);
+		}
+		catch (JDOMException jdomExc) {
+			System.err.println("Error: " + jdomExc.getMessage());
+			System.exit(-1);
+		}
+		
+	}
+
+	public static void main (String[] args) {
+		String usage = "Validator schemaFileLocation InstanceFileLocation";
+		if (args.length != 2) {
+			System.err.println("Usage:\n" + usage);
+			System.exit(-1);
+		}
+		new Validator(args[0], args[1]);
+
+	}
+
+}

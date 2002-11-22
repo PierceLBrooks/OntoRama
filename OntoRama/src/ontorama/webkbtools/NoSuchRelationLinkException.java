4f8264139416e892b328e19cb7b7cf13b9786859
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-11-22 04:44:16 +0000

    moved exceptions from util subpackage

---
+++ OntoRama/src/ontorama/webkbtools/NoSuchRelationLinkException.java
@@ -0,0 +1,36 @@
+package ontorama.webkbtools;
+
+
+/**
+
+ * Title:
+
+ * Description:
+
+ * Copyright:    Copyright (c) 2001
+
+ * Company:
+
+ * @author
+
+ * @version 1.0
+
+ */
+
+
+public class NoSuchRelationLinkException extends Exception {
+
+    private String errorMsg = null;
+
+    /**
+     * @todo  dont' need parameter maxValue, need to remove it.
+     */
+    public NoSuchRelationLinkException(int relationLink, int maxValue) {
+        errorMsg = "RelationLink " + relationLink + " does not exist. Enter a value between 0 and " + maxValue;
+    }
+
+    public NoSuchRelationLinkException (String linkName) {
+        errorMsg = "RelationLink " + linkName + "doesn't exist. Check configuration file";
+    }
+
+}

6d8924e9c89d611806afad85967a2d994da65c1b
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-11-25 00:36:35 +0000

    moved package ontorama.tree into ontorama.view.tree

--- trunk/OntoRama/src/ontorama/tree/model/OntoTreeModel.java
+++ trunk/OntoRama/src/ontorama/tree/model/OntoTreeModel.java
@@ -1,4 +1,4 @@
-package ontorama.tree.model;
+package ontorama.view.tree.model;
 
 import java.util.Iterator;
 
--- trunk/OntoRama/src/ontorama/tree/model/OntoTreeNode.java
+++ trunk/OntoRama/src/ontorama/tree/model/OntoTreeNode.java
@@ -1,4 +1,4 @@
-package ontorama.tree.model;
+package ontorama.view.tree.model;
 
 import java.util.Enumeration;
 import java.util.Iterator;

8e4ef30c83f9adfbf82e79ab82ce12ffeaca5d58
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-12-04 01:45:06 +0000

    changed the way InputEvents are handled

--- OntoRama/src/ontorama/views/tree/view/OntoTreeView.java
+++ OntoRama/src/ontorama/views/tree/view/OntoTreeView.java
@@ -1,6 +1,5 @@
 package ontorama.views.tree.view;
 
-import java.awt.Event;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -52,13 +51,6 @@ public class OntoTreeView extends JScrollPane implements KeyListener, MouseListe
 
     private Tree _tree;
 
-    private boolean KEY_IS_PRESSED = false;
-    private boolean MOUSE_IS_PRESSED = false;
-    private int pressedKey = -1;
-    private int pressedMouseButton = -1;
-    private KeyEvent curKeyEvent = null;
-    private MouseEvent curMouseEvent = null;
-
     /**
      * Constructor
      */
@@ -87,23 +79,14 @@ public class OntoTreeView extends JScrollPane implements KeyListener, MouseListe
 
         _treeView.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         _treeView.setEditable(false);
-        _treeView.putClientProperty("JTree.lineStyle", "Angled");
-
         _treeView.addMouseListener(this);
         _treeView.addKeyListener(this);
-
         ToolTipManager.sharedInstance().registerComponent(this._treeView);
-
         _treeView.setCellRenderer(new OntoTreeRenderer());
-
         unfoldTree(ontoTreeModel);
-
         setViewportView(_treeView);
-
         _treeView.putClientProperty("JTree.lineStyle", "Angled");
         _treeView.setScrollsOnExpand(true);
-
-        repaint();
         _treeView.repaint();
     }
 
@@ -127,144 +110,89 @@ public class OntoTreeView extends JScrollPane implements KeyListener, MouseListe
     /**
      * Implementation of valueChanged needed for interface
      * implementation of TreeSelectionListener.
-     * Overriten here so we can control single mouse clicks behaviour ourselves.
+     * Overwritten here so we can control single mouse clicks behaviour ourselves.
      * @param   e - TreeSelectionEvent
      */
     public void valueChanged(TreeSelectionEvent e) {
         OntoTreeNode node = (OntoTreeNode) _treeView.getLastSelectedPathComponent();
-        debug.message("TreeSelectionListener ******** node " + node);
         return;
     }
 
-    /**
-     *
-     */
     public void keyPressed(KeyEvent e) {
-        this.KEY_IS_PRESSED = true;
-        this.pressedKey = e.getModifiers();
-        this.curKeyEvent = e;
+        processInputEvent(e);
     }
 
-    /**
-     *
-     */
     public void keyReleased(KeyEvent e) {
-        this.pressedKey = -1;
-        this.KEY_IS_PRESSED = false;
-        this.curKeyEvent = null;
-        if (this.KEY_IS_PRESSED) {
-            notifyMouseKeyEvent(e, this.curMouseEvent);
-            return;
-        }
     }
 
-    /**
-     *
-     */
     public void keyTyped(KeyEvent e) {
     }
 
-    /**
-     *
-     */
     public void mousePressed(MouseEvent e) {
-        this.curMouseEvent = e;
-        this.MOUSE_IS_PRESSED = true;
-        this.pressedMouseButton = e.getModifiers();
         int selRow = _treeView.getRowForLocation(e.getX(), e.getY());
         if (selRow == -1) {
         	return;
         }
         if (e.getClickCount() == 1) {
-            debug.message("mousePressed, single click,  row=" + selRow);
-//            int onmask = InputEvent.BUTTON1_DOWN_MASK;
-//            int offmask = InputEvent.BUTTON2_DOWN_MASK | InputEvent.BUTTON3_DOWN_MASK |
-//                            InputEvent.ALT_DOWN_MASK | InputEvent.ALT_DOWN_MASK | InputEvent.CTRL_DOWN_MASK
-//                            | InputEvent.META_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK;
-//            if ( (onmask | offmask) == onmask) {
-//                TreeNode modelNode = getModelNodeFromMouseEvent(e);
-//                if (modelNode == null ) return;
-//                _eventBroker.processEvent(new TreeNodeSelectedEvent(modelNode, _eventBroker));
-//                System.out.println("HAHA");
-//                return;
-//            }
-//            onmask = InputEvent.BUTTON1_DOWN_MASK | InputEvent.CTRL_DOWN_MASK;
-//            offmask = InputEvent.BUTTON2_DOWN_MASK | InputEvent.BUTTON3_DOWN_MASK |
-//                            InputEvent.ALT_DOWN_MASK | InputEvent.ALT_DOWN_MASK
-//                            | InputEvent.META_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK;
-//            if ( (onmask | offmask) == onmask) {
-//                notifyMouseKeyEvent(this.curKeyEvent, e);
-//                System.out.println("HEHE");
-//                return;
-//            }
-            if (this.KEY_IS_PRESSED) {
-                notifyMouseKeyEvent(this.curKeyEvent, e);
-            } else {
-				TreeNode modelNode = getModelNodeFromMouseEvent(e);
-				if (modelNode == null ) return;
-                _eventBroker.processEvent(new TreeNodeSelectedEvent(modelNode, _eventBroker));
-            }
+            processInputEvent(e);
         }
     }
 
-    /**
-     *
-     */
+    private void processInputEvent(InputEvent e) {
+        TreeNode modelNode = getModelNodeFromInputEvent(e);
+        if (modelNode == null ) return;
+        int onmask = InputEvent.BUTTON1_DOWN_MASK;
+        //int offmask = InputEvent.BUTTON2_DOWN_MASK | InputEvent.BUTTON3_DOWN_MASK |
+        //                InputEvent.ALT_DOWN_MASK | InputEvent.ALT_DOWN_MASK | InputEvent.CTRL_DOWN_MASK
+        //                | InputEvent.META_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK;
+        if ( e.getModifiersEx() == onmask) {
+            _eventBroker.processEvent(new TreeNodeSelectedEvent(modelNode, _eventBroker));
+            return;
+        }
+        onmask = InputEvent.BUTTON1_DOWN_MASK | InputEvent.CTRL_DOWN_MASK;
+        //offmask = InputEvent.BUTTON2_DOWN_MASK | InputEvent.BUTTON3_DOWN_MASK |
+        //                InputEvent.ALT_DOWN_MASK | InputEvent.ALT_DOWN_MASK
+        //                | InputEvent.META_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK;
+        if ( e.getModifiersEx() == onmask) {
+//            _eventBroker.processEvent(new QueryEvent(graphNode));
+            System.out.println("query for modelNode = " + modelNode);
+            return;
+        }
+    }
+
     public void mouseClicked(MouseEvent e) {
     }
 
-    /**
-     *
-     */
     public void mouseEntered(MouseEvent e) {
     }
 
-    /**
-     *
-     */
     public void mouseExited(MouseEvent e) {
     }
 
-    /**
-     *
-     */
     public void mouseReleased(MouseEvent e) {
-        this.pressedMouseButton = -1;
-        this.MOUSE_IS_PRESSED = false;
-        this.curMouseEvent = null;
-    }
-
-    /**
-     *
-     */
-    private void notifyMouseKeyEvent(KeyEvent keyEvent, MouseEvent mouseEvent) {
-		ontorama.model.tree.TreeNode modelNode = getModelNodeFromMouseEvent(mouseEvent);
-		if (modelNode == null ) return;
-    	
-        int keyEventCode = keyEvent.getModifiers();
-        int mouseEventCode = mouseEvent.getModifiers();
-        System.out.println("notifyMouseKeyEvent, Event.META_MASK = " + Event.META_MASK + ", mouseEventCode = " + mouseEventCode);
-        //if ((keyEventCode == InputEvent.CTRL_MASK) && (mouseEventCode == InputEvent.BUTTON1_MASK)) {
-        if (keyEventCode == InputEvent.CTRL_MASK) {
-//            _eventBroker.processEvent(new QueryEvent(graphNode));
-        }
     }
     
-    
-    /**
-     * 
-     */
-    private ontorama.model.tree.TreeNode getModelNodeFromMouseEvent (MouseEvent mouseEvent) {
-        TreePath selPath = _treeView.getPathForLocation(mouseEvent.getX(), mouseEvent.getY());
-        if (selPath == null) {
-            // mouse clicked not on a node, but somewhere else in the tree ui
-            return null;
+    private ontorama.model.tree.TreeNode getModelNodeFromInputEvent (InputEvent event) {
+        if (event instanceof KeyEvent) {
+            OntoTreeNode treeNode = (OntoTreeNode) _treeView.getLastSelectedPathComponent();
+            if (treeNode == null) {
+                return null;
+            }
+            ontorama.model.tree.TreeNode modelNode = treeNode.getModelTreeNode();
+            return modelNode;
         }
-		return  getModelNodeFromTreePath(selPath);
+        if (event instanceof MouseEvent) {
+            MouseEvent mouseEvent = (MouseEvent) event;
+            TreePath selPath = _treeView.getPathForLocation(mouseEvent.getX(), mouseEvent.getY());
+            if (selPath == null) {
+                // mouse clicked not on a node, but somewhere else in the tree view
+                return null;
+            }
+            return  getModelNodeFromTreePath(selPath);
+        }
+        return null;
     }
    
-    
-
 	/**
 	 * 
 	 */

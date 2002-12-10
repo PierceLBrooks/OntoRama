9796fe71cc4df5c0764a9eb3033f5ee4ac779476
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-12-10 00:32:52 +0000

    first import

---
+++ OntoRama/src/ontorama/ui/controller/GeneralQueryEventHandler.java
@@ -0,0 +1,34 @@
+package ontorama.ui.controller;
+
+import org.tockit.events.EventBrokerListener;
+import org.tockit.events.Event;
+import org.tockit.events.EventBroker;
+import ontorama.ontotools.query.Query;
+import ontorama.ui.events.QueryStartEvent;
+import ontorama.ui.events.QueryEvent;
+import ontorama.ui.events.GeneralQueryEvent;
+import ontorama.model.GeneralNode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nataliya
+ * Date: Dec 9, 2002
+ * Time: 11:27:07 AM
+ * To change this template use Options | File Templates.
+ */
+public class GeneralQueryEventHandler implements EventBrokerListener {
+
+    private EventBroker _eventBroker;
+
+    public GeneralQueryEventHandler(EventBroker eventBroker) {
+        _eventBroker = eventBroker;
+        _eventBroker.subscribe(this, GeneralQueryEvent.class, Query.class);
+    }
+
+    public void processEvent(Event event) {
+        System.out.println("***GeneralQueryEventHandler processEvent()");
+        Query query = (Query) event.getSubject();
+        _eventBroker.processEvent(new QueryStartEvent(query));
+    }
+
+}

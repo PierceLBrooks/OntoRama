53cb7082fc89b3d2c1c3636f67d175899bc69c0f
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2002-12-18 07:55:03 +0000

    Now using only one backend at a time
    If multiple backends are specified - we are just taking the first one for now.
    Works for FileBackend so far

--- trunk/OntoRama/src/ontorama/backends/BackendSearch.java
+++ trunk/OntoRama/src/ontorama/backends/BackendSearch.java
@@ -1,8 +1,5 @@
 package ontorama.backends;
 
-import java.util.Iterator;
-
-import ontorama.backends.p2p.P2PBackend;
 import ontorama.backends.p2p.model.P2PGraph;
 import ontorama.backends.p2p.model.P2PGraphImpl;
 import ontorama.model.graph.GraphModificationException;
@@ -26,18 +23,12 @@ public class BackendSearch {
 	public static P2PGraph search(Query query){
         P2PGraph retVal = new P2PGraphImpl();
         try {
+            Peer2PeerBackend backend = (Peer2PeerBackend) OntoRamaApp.getBackend();
+            System.out.println("---searching backend " + backend + " for query " + query);
 
+            P2PGraph tempGraph = backend.search(query);
 
-            Iterator backendIt = OntoRamaApp.getBackends().iterator();
-
-            while (backendIt.hasNext()) {
-                Peer2PeerBackend backend = (Peer2PeerBackend) backendIt.next();
-                System.out.println("---searching backend " + backend + " for query " + query);
-
-                P2PGraph tempGraph = backend.search(query);
-
-                retVal.add(tempGraph);
-            }
+            retVal.add(tempGraph);
             System.out.println("returning " + retVal);
             System.out.println(" returned nodes: " + retVal.getNodesList());
             System.out.println(" returned edges: " + retVal.getEdgesList());
@@ -53,17 +44,11 @@ public class BackendSearch {
     public static P2PGraph searchLocal(Query query) {
         P2PGraph retVal = new P2PGraphImpl();
         try{
-            Iterator backendIt = OntoRamaApp.getBackends().iterator();
-            while (backendIt.hasNext()) {
-                Peer2PeerBackend backend = (Peer2PeerBackend) backendIt.next();
-                System.out.println("--1---searching backend " + backend + " for query " + query);
-                if (backend instanceof P2PBackend) {
-                    System.out.println("skipping P2PBackend backend");
-                    continue;
-                }
-                P2PGraph tempVal = backend.search(query);
-                retVal.add(tempVal);
-            }
+            Peer2PeerBackend backend = (Peer2PeerBackend) OntoRamaApp.getBackend();
+            System.out.println("--1---searching backend " + backend + " for query " + query);
+            P2PGraph tempVal = backend.search(query);
+            retVal.add(tempVal);
+
             System.out.println("----this backend returned nodes: " + retVal.getNodesList());
             System.out.println(" returned edges: " + retVal.getEdgesList());
             System.out.println(" returned nodes: " + retVal.getNodesList());

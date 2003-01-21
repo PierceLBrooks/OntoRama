22082f98a13f6de75b82b8c636ff509784a4d066
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2003-01-21 03:10:54 +0000

    Changed Writer Test so it doesn't depend on RdfDamlParser

---
+++ OntoRama/src/ontorama/ontotools/writer/rdf/test/Util.java
@@ -0,0 +1,45 @@
+package ontorama.ontotools.writer.rdf.test;
+
+import com.hp.hpl.mesa.rdf.jena.model.NsIterator;
+import com.hp.hpl.mesa.rdf.jena.model.RDFException;
+import com.hp.hpl.mesa.rdf.jena.model.ResIterator;
+import com.hp.hpl.mesa.rdf.jena.model.StmtIterator;
+
+/**
+ * @author nataliya
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class Util {
+	protected static int getStmtIteratorSize (StmtIterator it) throws RDFException {
+		int res = 0;
+		while (it.hasNext()) {
+			Object element = it.next();
+			res++;
+		}
+		return res;
+	}
+
+	protected static int getResIteratorSize (ResIterator it) throws RDFException {
+		int res = 0;
+		while (it.hasNext()) {
+			Object element = (Object) it.next();
+			res++;
+		}
+		return res;
+	}
+
+	protected static int getNsIteratorSize (NsIterator it) throws RDFException {
+		int res = 0;
+		while (it.hasNext()) {
+			Object element = it.next();
+			res++;
+		}
+		return res;
+	}
+	
+
+}

4e5b7ae1d2051fd145e72284d77d0a6fb521cd85
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2003-02-04 06:55:22 +0000

    first import

---
+++ trunk/OntoRama/src/ontorama/importer/Importer.java
@@ -0,0 +1,21 @@
+package ontorama.importer;
+
+/**
+ * @author nataliya
+ */
+public interface Importer {
+	
+	/**
+	 * name of the importer (appears as a label on the importer menu).
+	 * @return String
+	 */
+	public String getName();
+	
+	/**
+	 * this method will open necessary dialog
+	 */
+	public void doImport();
+	
+	
+
+}

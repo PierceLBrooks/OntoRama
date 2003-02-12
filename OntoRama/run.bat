d4875efc153621c42d480e250022079f8a694b04
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2003-02-12 01:23:53 +0000

    first import: batch files for demo

---
+++ OntoRama/run.bat
@@ -0,0 +1,1 @@
+java -cp lib/jdom.jar;lib/jenaOntorama.jar;lib/sax2beta.jar;lib/xerces.jar;lib/canvas.jar;lib/events.jar;lib/concurrent-1.3.0.jar;lib/jakarta-oro-2.0.5.jar;deploy ontorama.ui.OntoRamaApp

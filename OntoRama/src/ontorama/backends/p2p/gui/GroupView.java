30f448a7a99cf614927263f116a34fba47e94c36
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2003-02-13 06:39:08 +0000

    first import

---
+++ OntoRama/src/ontorama/backends/p2p/gui/GroupView.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 13/02/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code Template
+ */
+package ontorama.backends.p2p.gui;
+
+import ontorama.backends.p2p.p2pprotocol.GroupReferenceElement;
+
+/**
+ * @author nataliya
+ */
+public interface GroupView {
+	public void addGroup(GroupReferenceElement groupReferenceElement);
+//	public void removeGroup();
+}

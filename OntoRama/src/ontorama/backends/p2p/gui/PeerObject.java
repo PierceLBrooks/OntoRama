4e7c517566b38d773ad07c10b1a66132dc96be1d
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2003-03-05 23:37:28 +0000

    first import

---
+++ OntoRama/src/ontorama/backends/p2p/gui/PeerObject.java
@@ -0,0 +1,31 @@
+package ontorama.backends.p2p.gui;
+
+/**
+ * @author nataliya
+ * Created on 6/03/2003
+ */
+public class PeerObject {
+	// @todo this class here is only so we can keep track of peer names in relation 
+	// peer id's. ItemReference already does that - we should use it. For this to
+	// happen we need to figure out if it is possible to either pass ItemReference id's 
+	// from whoever gets that info from the network or if it is possible 
+	// to pass ID's instead of strings.
+
+	private String peerId;
+	private String peerName;
+
+	public PeerObject (String peerId, String peerName) {
+		this.peerId = peerId;
+		this.peerName = peerName;
+	}
+    	
+	public String getPeerId() {
+		return peerId;
+	}
+
+	public String getPeerName() {
+		return peerName;
+	}
+
+
+}

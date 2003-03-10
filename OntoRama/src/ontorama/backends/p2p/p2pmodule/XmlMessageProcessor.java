ed52011035217d8c6962f361b6e3a4bb8f8cb501
nataliya <nataliya@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2003-03-10 00:34:23 +0000

    XmlMessageProcessor now can read and write messages. Test cases have been updated accordinally.

--- OntoRama/src/ontorama/backends/p2p/p2pmodule/XmlMessageProcessor.java
+++ OntoRama/src/ontorama/backends/p2p/p2pmodule/XmlMessageProcessor.java
@@ -1,13 +1,21 @@
 package ontorama.backends.p2p.p2pmodule;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import ontorama.backends.p2p.model.Change;
 import ontorama.backends.p2p.model.EdgeChange;
@@ -68,7 +76,6 @@ public class XmlMessageProcessor {
 		
 
 		NodeList nodeElements = root.getElementsByTagName(NODE);
-		System.out.println("" + nodeElements + ", size = " + nodeElements.getLength());
 		if (nodeElements.getLength() != 0) {
 			Node nodeEl = nodeElements.item(0);
 
@@ -80,7 +87,7 @@ public class XmlMessageProcessor {
 		}
 		else {
 			NodeList edgeElements = root.getElementsByTagName(EDGE);
-			Node edgeEl = nodeElements.item(0);
+			Node edgeEl = edgeElements.item(0);
 
 			NamedNodeMap edgeAttr = edgeEl.getAttributes();
 			String fromNodeAttr = edgeAttr.getNamedItem(FROM).getNodeValue();
@@ -90,6 +97,54 @@ public class XmlMessageProcessor {
 			result = new EdgeChange(fromNodeAttr, toNodeAttr, edgeTypeAttr, actionTypeAttr, initiatorAttr);
 		}
 		return result;
+	}
+	
+	public static String createMessage(Change modelChange) throws 
+											ParserConfigurationException,
+											TransformerConfigurationException,
+											TransformerException {
+		String res = "";
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.newDocument();
+		
+		Element root = document.createElement(MESSAGE);
+		
+		Element actionNode = document.createElement(ACTION);
+		actionNode.setAttribute(INITIATOR_URI, modelChange.getInitiatorUri());
+		actionNode.setAttribute(TYPE, modelChange.getAction());
+		
+		document.appendChild(root);
+		root.appendChild(actionNode);
+		
+		Element itemNode = null;
+		if (modelChange instanceof NodeChange) {
+			NodeChange nodeChange = (NodeChange) modelChange;
+			itemNode = document.createElement(NODE);
+			itemNode.setAttribute(IDENTIFIER, nodeChange.getNodeName());
+			itemNode.setAttribute(TYPE, nodeChange.getNodeType());
+		}
+		else {
+			EdgeChange edgeChange = (EdgeChange) modelChange;
+			itemNode = document.createElement(EDGE);
+			itemNode.setAttribute(FROM, edgeChange.getFromNode());
+			itemNode.setAttribute(TO, edgeChange.getToNode());
+			itemNode.setAttribute(TYPE, edgeChange.getEdgeType());
+		}
+		root.appendChild(itemNode);
+		
+		DOMSource domSource = new DOMSource(document);
+		TransformerFactory tFactory = TransformerFactory.newInstance();
+		Transformer transformer = tFactory.newTransformer();
+
+		OutputStream outStream = new ByteArrayOutputStream();
+		StreamResult result = new StreamResult(outStream);
+		transformer.transform(domSource, result);
+		
+		res = outStream.toString();		
+		System.out.println("stream result = " + res);
+		return res;
 	}
 	
 }
--- OntoRama/src/ontorama/backends/p2p/p2pmodule/test/TestXmlMessageProcessor.java
+++ OntoRama/src/ontorama/backends/p2p/p2pmodule/test/TestXmlMessageProcessor.java
@@ -28,37 +28,70 @@ public class TestXmlMessageProcessor extends TestCase {
     }
 
     protected void setUp()  {
-		_modelChangeNode = "<message>" +
+		_modelChangeNode = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+				"<message>" +
 				"<action type=\"assert\" initiatorUri=\"someone@ontorama.org\" />" + 
 				"<node identifier=\"node1\" type=\"CONCEPT_TYPE\"/>" +
 				"</message>";		
     	_nodeChange = new NodeChange("node1","CONCEPT_TYPE","assert","someone@ontorama.org");
 
-		_modelChangeEdge = "<message>" +
+		_modelChangeEdge = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+				"<message>" +
 				"<action type=\"reject\" initiatorUri=\"someone@ontorama.org\" />" + 
 				"<edge fromNodeIdentifier=\"node1\" toNodeIdentifier=\"node2\" type=\"subtype\"/>" +
 				"</message>";		
 
-		_edgeChange = new EdgeChange("node1", "node2", "subtype", "reject", "someone@otnorama.org");
+		_edgeChange = new EdgeChange("node1", "node2", "subtype", "reject", "someone@ontorama.org");
     }
 
     public void testParsingNode () throws Exception {
     	Change change = XmlMessageProcessor.parseXmlMessage(_modelChangeNode);
     	NodeChange res = (NodeChange) change;
-    	assertEquals(_nodeChange.getAction(), res.getAction());
-    	assertEquals(_nodeChange.getInitiatorUri(), res.getInitiatorUri());
-    	assertEquals(_nodeChange.getNodeName(), res.getNodeName());
-    	assertEquals(_nodeChange.getNodeType(), res.getNodeType());
+		checkNodeChange(_nodeChange, res);
     }
+
     
     public void testParsingEdge () throws Exception {
     	Change change = XmlMessageProcessor.parseXmlMessage(_modelChangeEdge);
     	EdgeChange res = (EdgeChange) change;
-		assertEquals(_edgeChange.getAction(), res.getAction());
-		assertEquals(_edgeChange.getInitiatorUri(), res.getInitiatorUri());
-		assertEquals(_edgeChange.getFromNode(), res.getFromNode());
-		assertEquals(_edgeChange.getToNode(), res.getToNode());
-		assertEquals(_edgeChange.getEdgeType(), res.getEdgeType());
+		checkEdgeChange(_edgeChange, res);
     }
+
+    
+    public void testCreateNodeMessage() throws Exception {
+    	String message = XmlMessageProcessor.createMessage(_nodeChange);
+    	
+    	// read and parse resulting string and check if 
+    	// resulting NodeChange is the same as our original node change 
+		Change change = XmlMessageProcessor.parseXmlMessage(message);
+		NodeChange res = (NodeChange) change;
+    	checkNodeChange(_nodeChange, res);
+    }
+
+
+	public void testCreateEdgeMessage() throws Exception {
+		String message = XmlMessageProcessor.createMessage(_edgeChange);
+    	
+		// read and parse resulting string and check if 
+		// resulting EdgeChange is the same as our original edge change 
+		Change change = XmlMessageProcessor.parseXmlMessage(message);
+		EdgeChange res = (EdgeChange) change;
+		checkEdgeChange(_edgeChange, res);
+	}
+
+	private void checkNodeChange(NodeChange expected, NodeChange result) {
+		assertEquals(expected.getAction(), result.getAction());
+		assertEquals(expected.getInitiatorUri(), result.getInitiatorUri());
+		assertEquals(expected.getNodeName(), result.getNodeName());
+		assertEquals(expected.getNodeType(), result.getNodeType());
+	}
+
+	private void checkEdgeChange(EdgeChange expected, EdgeChange res) {
+		assertEquals(expected.getAction(), res.getAction());
+		assertEquals(expected.getInitiatorUri(), res.getInitiatorUri());
+		assertEquals(expected.getFromNode(), res.getFromNode());
+		assertEquals(expected.getToNode(), res.getToNode());
+		assertEquals(expected.getEdgeType(), res.getEdgeType());
+	}
 
 }

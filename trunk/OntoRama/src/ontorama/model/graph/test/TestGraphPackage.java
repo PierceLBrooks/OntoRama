4a46e22d5db76964d3b7a1aa529e984073534a6d
peterbecker <peterbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2009-03-16 07:09:48 +0000

    A couple of more cleanup changes.
    Jumping a bit over the place at the moment:
     * more moves towards a Maven like layout
     * removal of the Test*Package TestCases (IDEs/build tools do that anyway)
     * removal of some old comments
     
    After this (and possibly before) the Ant build file will be broken. Since I am aiming for Maven and have a working Eclipse config for development purposes I accept it for now.

--- trunk/OntoRama/src/ontorama/model/graph/test/TestGraphPackage.java
+++ trunk/OntoRama/src/ontorama/model/graph/test/TestGraphPackage.java
@@ -1,8 +1,5 @@
 package ontorama.model.graph.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * <p>Title: </p>
@@ -13,23 +10,10 @@ import junit.framework.TestSuite;
  * @version 1.0
  */
 
-public class TestGraphPackage extends TestCase {
+public class TestGraphPackage {
 
 	public static final String edgeName_subtype = "subtype";
 	public static final String edgeName_similar = "similar";
 	public static final String edgeName_reverse = "reverse";
 	
-
-    public TestGraphPackage(String name) {
-        super(name);
-    }
-
-
-    public static Test suite() {
-        TestSuite suite = new TestSuite("ontorama.model.graph");
-        suite.addTest(new TestSuite(TestEdge.class));
-        suite.addTest(new TestSuite(TestNode.class));
-        suite.addTest(new TestSuite(TestGraph.class));
-        return suite;
-    }
 }

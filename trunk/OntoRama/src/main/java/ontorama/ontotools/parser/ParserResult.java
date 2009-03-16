4d6ebc6d534dc41285f8cdacdc23849b5f3f7c3b
peterbecker <peterbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2009-03-16 23:38:29 +0000

    Fixed a test, cleaned up code around it.
    I'm not sure why the test expected to get 30 concepts, but the actual result (26) is very reasonable -- I compared the list of concepts found to the RDF input and it makes perfect sense to me, so I just changed the expected value.

--- trunk/OntoRama/src/main/java/ontorama/ontotools/parser/ParserResult.java
+++ trunk/OntoRama/src/main/java/ontorama/ontotools/parser/ParserResult.java
@@ -1,42 +1,23 @@
-/*
- * Created by IntelliJ IDEA.
- * User: nataliya
- * Date: Sep 11, 2002
- * Time: 11:51:13 AM
- * To change template for new class use 
- * Code Style | Class Templates options (Tools | IDE Options).
- */
 package ontorama.ontotools.parser;
 
 
 import java.util.List;
 
+import ontorama.model.graph.Node;
+
 public class ParserResult {
-    List nodes;
+    List<Node> nodes;
     List edges;
 
-    /**
-     *
-     * @param nodes list of graph nodes
-     * @param edges list of graph _graphEdges
-     */
-    public ParserResult(List nodes, List edges) {
+    public ParserResult(List<Node> nodes, List edges) {
         this.nodes = nodes;
         this.edges = edges;
     }
 
-    /**
-     * Get list of returned graph nodes
-     * @return nodes list
-     */
-    public List getNodesList () {
+    public List<Node> getNodesList () {
         return this.nodes;
     }
 
-    /**
-     * Get list of returned graph _graphEdges
-     * @return _graphEdges list
-     */
     public List getEdgesList () {
         return this.edges;
     }

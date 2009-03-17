3da20d0fc4a7535ef7196adf5eb0204b1691b786
peterbecker <peterbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2009-03-17 04:51:05 +0000

    Removed author/version tags.
    Our VCS can tell us.

--- trunk/OntoRama/src/test/java/ontorama/ontotools/parser/rdf/TestRdfDamlParser.java
+++ trunk/OntoRama/src/test/java/ontorama/ontotools/parser/rdf/TestRdfDamlParser.java
@@ -30,8 +30,6 @@ import ontorama.ontotools.source.Source;
  * </p>
  * <p>Copyright: Copyright (c) 2002</p>
  * <p>Company: DSTC</p>
- * @author nataliya
- * @version 1.0
  */
 public class TestRdfDamlParser extends TestCase {
 

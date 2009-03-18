67bf176e5f0dbfa7ad98cbb0e2ed6cc56b9c8ef8
peterbecker <peterbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2009-03-18 01:20:03 +0000

    Cleaned up comments.

--- trunk/OntoRama/src/main/java/ontorama/ontotools/source/JarSource.java
+++ trunk/OntoRama/src/main/java/ontorama/ontotools/source/JarSource.java
@@ -8,21 +8,14 @@ import ontorama.ontotools.SourceException;
 import ontorama.ontotools.query.Query;
 
 /**
- * <p>Title: </p>
- * <p>Description: </p>
  * <p>Copyright: Copyright (c) DSTC 2002</p>
  * <p>Company: DSTC</p>
- * @version 1.0
  */
-
-
 public class JarSource implements Source {
     /**
      *  Get a SourceResult from a given location in a jar file.
      *  This will work for any location relative to Class Loader.
      *  @param  relativePath  path relative to ClassLoader
-     *  @return SourceResult
-     *  @throws SourceException
      *
      * @todo implement if needed: throw CancelledQueryException
      */
@@ -32,12 +25,7 @@ public class JarSource implements Source {
         return new SourceResult(true, reader, null);
     }
 
-	/**
-	 * @todo check if need this class at all.
-	 */
     public InputStream getInputStreamFromResource(String resourceName) throws SourceException {
 		return this.getClass().getClassLoader().getResourceAsStream(resourceName);
     }
-
-
 }

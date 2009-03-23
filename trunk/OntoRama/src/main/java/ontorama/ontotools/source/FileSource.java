3ae6c12a42e4935b304690df00388cdd47c2ee2c
peterbecker <peterbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2009-03-23 03:25:26 +0000

    More comments removed due to lack of information.

--- trunk/OntoRama/src/main/java/ontorama/ontotools/source/FileSource.java
+++ trunk/OntoRama/src/main/java/ontorama/ontotools/source/FileSource.java
@@ -8,22 +8,9 @@ import ontorama.ontotools.CancelledQueryException;
 import ontorama.ontotools.SourceException;
 import ontorama.ontotools.query.Query;
 
-/*
- * Created by IntelliJ IDEA.
- * User: nataliya
- * Date: Oct 23, 2002
- * Time: 11:26:30 AM
- * To change this template use Options | File Templates.
- */
 public class FileSource implements Source {
 
-    /**
-     *
-     * @param filename
-     * @param query
-     * @return
-     * @throws SourceException
-     * @throws CancelledQueryException
+    /*
      * @todo implement if needed: throw CancelledQueryException
      */
     public SourceResult getSourceResult(String filename, Query query) throws SourceException, CancelledQueryException {

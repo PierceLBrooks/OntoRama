fd7de14d1c172e621f607a3269f19c55c77b6223
peterbecker <peterbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2009-03-23 04:32:15 +0000

    Removed useless comment.

--- trunk/OntoRama/src/main/java/ontorama/model/GeneralNode.java
+++ trunk/OntoRama/src/main/java/ontorama/model/GeneralNode.java
@@ -1,12 +1,5 @@
 package ontorama.model;
 
-/**
- * Created by IntelliJ IDEA.
- * User: nataliya
- * Date: Dec 4, 2002
- * Time: 12:10:49 PM
- * To change this template use Options | File Templates.
- */
 public interface GeneralNode {
     public String getName();
 }

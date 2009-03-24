77ff9cbabca7b62290481304d6a95a3d207504a3
peterbecker <peterbecker@abaabd80-4e4a-0410-a02f-a5a77c7f326d>
2009-03-24 00:54:46 +0000

    Turned antialiasing on for image generation.
    This improves things a little bit -- the scaled images still look crusty, but not entirely as crusty as before. It certainly shouldn't hurt performance, though -- the images are all prerendered.

--- OntoRama/src/main/java/ontorama/conf/ImageMaker.java
+++ OntoRama/src/main/java/ontorama/conf/ImageMaker.java
@@ -2,29 +2,21 @@ package ontorama.conf;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
 
 public class ImageMaker {
 
     private static int height = 15;
     private static int width = 20;
 
-
     public static Image getImage(Color color, String symbol) {
-        Image image = new BufferedImage(ImageMaker.width, ImageMaker.height, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(ImageMaker.width, ImageMaker.height, BufferedImage.TYPE_INT_RGB);
 
-        Graphics g = image.getGraphics();
+        Graphics2D g = image.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setColor(color);
         g.fillRect(0, 0, ImageMaker.width, ImageMaker.height);
         g.setColor(Color.black);
@@ -39,16 +31,10 @@ public class ImageMaker {
         return image;
     }
 
-    /**
-     *
-     */
     public static int getWidth() {
         return ImageMaker.width;
     }
 
-    /**
-     *
-     */
     public static int getHeight() {
         return ImageMaker.height;
     }
